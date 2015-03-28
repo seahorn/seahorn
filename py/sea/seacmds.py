ba149dc14a92815b5f90e9552c6d702dd63f323d
Arie Gurfinkel <arie@cmu.edu>
2015-03-28 01:16:10 -0400

    Additional commands seapp, seaopt, mixed-sem

--- py/sea/seacmds.py
+++ py/sea/seacmds.py
@@ -15,7 +15,7 @@ def _remap_file_name (in_file, ext, work_dir):
     
 class Clang(CliCmd.LimitedCmd):
     def __init__ (self, quiet=False):
-        super (Clang, self).__init__('clang')
+        super (Clang, self).__init__('clang', allow_extra=True)
         cmd_name = util.which (['clang-mp-3.6', 'clang-3.6', 'clang',
                                 'clang-mp-3.5', 'clang-mp-3.4'])
         if cmd_name is None: raise IOError ('clang not found')
@@ -50,3 +50,93 @@ class Clang(CliCmd.LimitedCmd):
     @property
     def stdout (self):
         return self.clangCmd.stdout
+
+class Seapp(CliCmd.LimitedCmd):
+    def __init__(self, quiet=False):
+        super(CliCmd, self).__init__('seapp', allow_extra=True)
+        cmd_name = util.which ('seapp')
+        if cmd_name is None: raise IOError ('seapp not found')
+        self.seappCmd = CliCmd.ExtCmd (cmd_name)
+
+    @property
+    def stdout (self):
+        return self.seappCmd.stdout
+        
+    def name_out_file (self, in_file, work_dir=None):
+        return _remap_file_name (in_file, '.pp.bc', work_dir)
+
+    def mk_arg_parser (self, ap):
+        ap = super (Seapp, self).mk_arg_parser (ap)
+        ap.add_argument ('--inline', dest='inline', help='Inline all functions',
+                         default=False, action='store_true')
+        ap.add_argument ('-o', dest='out_file',
+                         metavar='FILE', help='Output file name', default=None)
+        ap.add_argument ('in_file',  metavar='FILE', help='Input file')
+        return ap
+    
+    def run (self, args, extra):
+        argv = list()
+        if args.out_file is not None:
+            argv.extend (['-o', args.out_file])
+        if args.inline: argv.append ('--horn-inline-all')
+        return self.seappCmd.run (args, argv)
+        
+class MixedSem(CliCmd.CliCmd):
+    def __init__(self, quiet=False):
+        super(CliCmd, self).__init__('seapp', allow_extra=True)
+        cmd_name = util.which ('seapp')
+        if cmd_name is None: raise IOError ('seapp not found')
+        self.seappCmd = CliCmd.ExtCmd (cmd_name)
+
+    @property
+    def stdout (self):
+        return self.seappCmd.stdout
+        
+    def name_out_file (self, in_file, work_dir=None):
+        return _remap_file_name (in_file, '.ms.bc', work_dir)
+
+    def mk_arg_parser (self, ap):
+        ap = super (Seapp, self).mk_arg_parser (ap)
+        ap.add_argument ('--ms-skip', dest='ms_skip', help='Skip mixed semantics',
+                         default=False, action='store_true')
+        ap.add_argument ('-o', dest='out_file',
+                         metavar='FILE', help='Output file name', default=None)
+        ap.add_argument ('in_file',  metavar='FILE', help='Input file')
+        return ap
+    
+    def run (self, args, extra):
+        argv = list()
+        if args.out_file is not None: argv.extend (['-o', args.out_file])
+        if not args.ms_skip: argv.append ('--horn-mixed-sem')
+        return self.seappCmd.run (args, argv)
+
+class Seaopt(CliCmd.LimitedCmd):
+    def __init__(self, quiet=False):
+        super(CliCmd, self).__init__('seaopt', allow_extra=True)
+        cmd_name = util.which (['seaopt', 'opt-mp-3.6', 'opt-3.6', 'opt'])
+        if cmd_name is None: raise IOError ('niether seaopt nor opt where found')
+        self.seaoptCmd = CliCmd.ExtCmd (cmd_name)
+
+    @property
+    def stdout (self):
+        return self.seaoptCmd.stdout
+        
+    def name_out_file (self, in_file, work_dir=None):
+        return _remap_file_name (in_file, '.o.bc', work_dir)
+
+    def mk_arg_parser (self, ap):
+        ap = super (Seapp, self).mk_arg_parser (ap)
+        ap.add_argument ('-O', type=int, dest='opt_level', metavar='INT',
+                         help='Optimization level L:[0,1,2,3]', default=3)
+        ap.add_argument ('-o', dest='out_file',
+                         metavar='FILE', help='Output file name', default=None)
+        ap.add_argument ('in_file',  metavar='FILE', help='Input file')
+        return ap
+    
+    def run (self, args, extra):
+        argv = ['-f', '-funit-at-a-atime']
+        if args.out_file is not None:
+            argv.extend (['-o', args.out_file])
+        if args.opt_level > 0 && args.opt_level <= 3:
+            argv.extend (['-O', str(args.opt_level)])
+        return self.seappCmd.run (args, argv)

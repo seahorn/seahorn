68e35cd60199f31b790089a9580061569de24166
Arie Gurfinkel <arie@cmu.edu>
2015-03-28 10:55:44 -0400

    more commands and more fixes

--- py/sea/seacmds.py
+++ py/sea/seacmds.py
@@ -12,7 +12,18 @@ def _remap_file_name (in_file, ext, work_dir):
     out_file = os.path.splitext (out_file)[0]
     out_file = out_file + ext
     return out_file
-    
+
+def _add_in_out_args (ap):
+    ap.add_argument ('-o', dest='out_file',
+                     metavar='FILE', help='Output file name', default=None)
+    ap.add_argument ('in_file',  metavar='FILE', help='Input file')
+    return ap
+
+def _add_S_arg (ap):
+    ap.add_argument ('-S', dest='llvm_asm', default=False, action='store_true',
+                     help='Write output as LLVM assembly')
+    return ap
+
 class Clang(CliCmd.LimitedCmd):
     def __init__ (self, quiet=False):
         super (Clang, self).__init__('clang', allow_extra=True)
@@ -28,16 +39,18 @@ class Clang(CliCmd.LimitedCmd):
                          help='Machine architecture MACHINE:[32,64]', default=32)
         ap.add_argument ('-g', default=False, action='store_true',
                          dest='debug_info', help='Compile with debug info')
-        ap.add_argument ('-o', dest='out_file',
-                         metavar='FILE', help='Output file name', default=None)
-        ap.add_argument ('in_file',  metavar='FILE', help='Input file')
+        _add_in_out_args (ap)
+        _add_S_arg (ap)
         return ap
     
-    def name_out_file (self, in_file, work_dir=None):
-        return _remap_file_name (in_file, '.bc', work_dir)
+    def name_out_file (self, in_file, args=None, work_dir=None):
+        ext = '.bc'
+        if args.llvm_asm: ext = '.ll'
+        return _remap_file_name (in_file, ext, work_dir)
         
     def run (self, args, extra):
         argv = ['-c', '-emit-llvm']
+        if args.llvm_asm: argv.append ('-S')
         if args.machine == 32: argv.extend (['-arch', 'i386'])
         elif args.machine == 64: argv.extend (['-arch', 'x86_64'])
         
@@ -62,28 +75,29 @@ class Seapp(CliCmd.LimitedCmd):
     def stdout (self):
         return self.seappCmd.stdout
         
-    def name_out_file (self, in_file, work_dir=None):
-        return _remap_file_name (in_file, '.pp.bc', work_dir)
+    def name_out_file (self, in_file, args=None, work_dir=None):
+        ext = '.pp.bc'
+        if args.llvm_asm: ext = '.pp.ll'
+        return _remap_file_name (in_file, ext, work_dir)
 
     def mk_arg_parser (self, ap):
         ap = super (Seapp, self).mk_arg_parser (ap)
         ap.add_argument ('--inline', dest='inline', help='Inline all functions',
                          default=False, action='store_true')
-        ap.add_argument ('-o', dest='out_file',
-                         metavar='FILE', help='Output file name', default=None)
-        ap.add_argument ('in_file',  metavar='FILE', help='Input file')
+        _add_in_out_args (ap)
+        _add_S_arg (ap)
         return ap
     
     def run (self, args, extra):
         argv = list()
-        if args.out_file is not None:
-            argv.extend (['-o', args.out_file])
+        if args.out_file is not None: argv.extend (['-o', args.out_file])
         if args.inline: argv.append ('--horn-inline-all')
+        argv.append (args.in_file)
         return self.seappCmd.run (args, argv)
         
 class MixedSem(CliCmd.CliCmd):
     def __init__(self, quiet=False):
-        super(CliCmd, self).__init__('seapp', allow_extra=True)
+        super(MixedSem, self).__init__('seapp', allow_extra=True)
         cmd_name = util.which ('seapp')
         if cmd_name is None: raise IOError ('seapp not found')
         self.seappCmd = CliCmd.ExtCmd (cmd_name)
@@ -92,27 +106,29 @@ class MixedSem(CliCmd.CliCmd):
     def stdout (self):
         return self.seappCmd.stdout
         
-    def name_out_file (self, in_file, work_dir=None):
-        return _remap_file_name (in_file, '.ms.bc', work_dir)
+    def name_out_file (self, in_file, args=None, work_dir=None):
+        ext = '.ms.bc'
+        if args.llvm_asm: ext = '.ms.ll'
+        return _remap_file_name (in_file, ext, work_dir)
 
     def mk_arg_parser (self, ap):
-        ap = super (Seapp, self).mk_arg_parser (ap)
+        ap = super (MixedSem, self).mk_arg_parser (ap)
         ap.add_argument ('--ms-skip', dest='ms_skip', help='Skip mixed semantics',
                          default=False, action='store_true')
-        ap.add_argument ('-o', dest='out_file',
-                         metavar='FILE', help='Output file name', default=None)
-        ap.add_argument ('in_file',  metavar='FILE', help='Input file')
+        _add_in_out_args (ap)
+        _add_S_arg (ap)
         return ap
     
     def run (self, args, extra):
         argv = list()
         if args.out_file is not None: argv.extend (['-o', args.out_file])
         if not args.ms_skip: argv.append ('--horn-mixed-sem')
+        argv.append (args.in_file)
         return self.seappCmd.run (args, argv)
 
 class Seaopt(CliCmd.LimitedCmd):
     def __init__(self, quiet=False):
-        super(CliCmd, self).__init__('seaopt', allow_extra=True)
+        super(Seaopt, self).__init__('seaopt', allow_extra=True)
         cmd_name = util.which (['seaopt', 'opt-mp-3.6', 'opt-3.6', 'opt'])
         if cmd_name is None: raise IOError ('niether seaopt nor opt where found')
         self.seaoptCmd = CliCmd.ExtCmd (cmd_name)
@@ -121,16 +137,17 @@ class Seaopt(CliCmd.LimitedCmd):
     def stdout (self):
         return self.seaoptCmd.stdout
         
-    def name_out_file (self, in_file, work_dir=None):
-        return _remap_file_name (in_file, '.o.bc', work_dir)
+    def name_out_file (self, in_file, args, work_dir=None):
+        ext = 'o{0}.bc'.format(args.opt_level)
+        if args.llvm_asm: ext = 'o{0}.ll'.format(args.opt_level)
+        return _remap_file_name (in_file, ext, work_dir)
 
     def mk_arg_parser (self, ap):
-        ap = super (Seapp, self).mk_arg_parser (ap)
+        ap = super (Seaopt, self).mk_arg_parser (ap)
         ap.add_argument ('-O', type=int, dest='opt_level', metavar='INT',
                          help='Optimization level L:[0,1,2,3]', default=3)
-        ap.add_argument ('-o', dest='out_file',
-                         metavar='FILE', help='Output file name', default=None)
-        ap.add_argument ('in_file',  metavar='FILE', help='Input file')
+        _add_in_out_args (ap)
+        _add_S_arg (ap)
         return ap
     
     def run (self, args, extra):
@@ -139,4 +156,69 @@ class Seaopt(CliCmd.LimitedCmd):
             argv.extend (['-o', args.out_file])
         if args.opt_level > 0 && args.opt_level <= 3:
             argv.extend (['-O', str(args.opt_level)])
+        argv.append (args.in_file)
         return self.seappCmd.run (args, argv)
+
+class Seahorn(CilCmd.LimitedCmd):
+    def __init__ (self, quiet=False):
+        super (Seahorn, self).__init__ ('seahorn', allow_extra=True)
+        cmd_name = util.which ('seahorn')
+        if cmd_name is None: raise IOError ('seahorn not found')
+        self.seahornCmd = CliCmd.ExtCmd (cmd_name)
+        
+    @property
+    def stdout (self):
+        return self.seahornCmd.stdout
+
+    def name_out_file (self, in_file, args=None, work_dir=None):
+        return _remap_file_name (in_file, '.smt2', work_dir)
+
+    def mk_arg_parser (self, ap):
+        ap = super (Seahorn, self).mk_arg_parser (ap)
+        _add_in_out_args (ap)
+        ap.add_argument ('--cex', dest='cex', help='Destination for a cex',
+                         default=None, metavar='FILE')
+        ap.add_argument ('--solve', dest='solve', help='Solve', default=False)
+        ap.add_argument ('--ztrace', dest='ztrace', metavar='LVL[:LVL]*',
+                         default=None, help='Z3 trace levels')
+        ap.add_argument ('--verbose', '-v', dest='verbose', type=int, default=0,
+                         help='Verbosity level')
+        ap.add_argument ('--log', dest='log', default=None, help='Log level')
+        ap.add_argument ('--oll', dest='asm_out_file', default=None,
+                         help='LLVM assembly output file')
+        ### TODO: expose options for semantic level, inter-procedural
+        ### encoding, step, flat, etc.
+        return ap
+    def _is_seahorn_opt (x):
+        if x.startswith ('-'):
+            y = x.strip ('-')
+            return y.startswith ('horn') or y.startswith ('ikos') or y.startswith ('log')
+        return False
+
+    def run (self, args, extra):
+        argv = list()
+        if args.solve:
+            argv.append ('-horn-solve')
+        if args.cex is not None && args.solve:
+            argv.append ('-horn-cex')
+            argv.append ('--honr-svcomp-cex={0}'.format (cex))
+            argv.extend (['-log', 'cex'])
+        if args.asm_out_file is not None: argv.extend (['-oll', args.asm_out_file])
+        
+        argv.extend (['-horn-inter-proc', '-horn-sem-lvl=mem', '-horn-step=large'])
+        
+        if args.verbose > 0: argv.extend (['-zverbose', str(args.verbose)])
+
+        if args.log is not None:
+            for l in args.log.split (':'): argv.extend (['-log', l])
+        if args.ztrace is not None:
+            for l in args.ztrace.split (':'): argv.extend (['-ztrace', l])
+        
+        if args.out_file is not None: argv.extend (['-o', args.out_file])
+        argv.append (args.in_file)
+        
+        # pick out extra seahorn options
+        argv.extend (filter (_is_seahorn_opt, extra))
+        
+            
+        return self.seahornCmd.run (args, argv)

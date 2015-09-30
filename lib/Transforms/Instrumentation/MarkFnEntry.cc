c34127659fb5c447ffdeebfd42f7ee69d1cabb30
Arie Gurfinkel <arie@cmu.edu>
2015-09-29 21:54:02 -0400

    [FIX] forgot to commit MarkFnEntry pass

---
+++ lib/Transforms/Instrumentation/MarkFnEntry.cc
@@ -0,0 +1,97 @@
+/**
+SeaHorn Verification Framework
+Copyright (c) 2015 Carnegie Mellon University.
+All Rights Reserved.
+
+THIS SOFTWARE IS PROVIDED "AS IS," WITH NO WARRANTIES
+WHATSOEVER. CARNEGIE MELLON UNIVERSITY EXPRESSLY DISCLAIMS TO THE
+FULLEST EXTENT PERMITTEDBY LAW ALL EXPRESS, IMPLIED, AND STATUTORY
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND
+NON-INFRINGEMENT OF PROPRIETARY RIGHTS.
+
+Released under a modified BSD license, please see license.txt for full
+terms.
+
+DM-0002198
+*/
+
+#define DEBUG_TYPE "mark-fn-entry"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Support/Compiler.h"
+#include "llvm/PassManager.h"
+
+#include "llvm/IR/IRBuilder.h"
+
+
+using namespace llvm;
+
+namespace 
+{
+  /// Marks entry point of every function with a special call
+  class MarkFnEntry : public ModulePass
+  {
+    Constant *m_mark;
+    
+  public:
+    static char ID;
+    MarkFnEntry () : ModulePass (ID), m_mark(NULL) {} 
+    
+    virtual bool runOnModule (Module &M)
+    {
+      
+      m_mark = M.getOrInsertFunction ("seahorn.fn.enter",
+                                      Type::getVoidTy (M.getContext ()), NULL);
+      for (auto &F : M) runOnFunction (F);
+      return true;
+    }
+    virtual bool runOnFunction (Function &F)
+    {
+      if (F.empty ()) return false;
+      
+      // -- insert call
+      IRBuilder<> Builder (F.getContext ());
+      Builder.SetInsertPoint (&F.getEntryBlock (),
+                              F.getEntryBlock ().begin ());
+      CallInst *call = Builder.CreateCall (m_mark);
+      
+      
+      // -- find debug information if available
+      for (auto &BB : F)
+      {
+        auto Inst = std::find_if(BB.begin(), BB.end(), [](const Instruction &Inst) {
+            return !Inst.getDebugLoc().isUnknown();
+          });
+        if (Inst == BB.end())
+          continue;      
+
+        // -- found instruction with a good debug location
+        call->setDebugLoc (Inst->getDebugLoc ());
+        return true;
+      }
+      
+      return true;
+    }
+    
+    virtual void getAnalysisUsage (AnalysisUsage &AU) const
+    {AU.setPreservesAll ();}
+    
+  };
+  
+  char MarkFnEntry::ID = 0;
+}
+
+namespace seahorn
+{
+  llvm::Pass* createMarkFnEntryPass () {return new MarkFnEntry ();}
+}
+
+
+static RegisterPass<MarkFnEntry> X("mark-fn-enter", 
+                                   "Mark function entry point");
+
+

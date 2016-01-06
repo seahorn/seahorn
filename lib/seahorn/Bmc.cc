995c329960d55a33a80e322ded099f8509416944
Arie Gurfinkel <arie@cmu.edu>
2016-01-06 13:32:45 -0500

    a little more bmc encoding

--- lib/seahorn/Bmc.cc
+++ lib/seahorn/Bmc.cc
@@ -18,19 +18,36 @@ namespace seahorn
   boost::tribool BmcEngine::solve ()
   {
     encode ();
-    // m_result =  m_solver.solve ();
+    //m_result =  m_solver.solve ();
     return m_result;
   }
 
   void BmcEngine::encode ()
   {
-    // -- only run encoding once
+    
+    // -- only run the encoding once
     if (!m_side.empty ()) return;
     
+    assert (m_cpg);
+    assert (m_fn);
     UfoLargeSymExec sexec (m_sem);
     m_states.push_back (SymStore (m_efac));
-    // XXX TODO 
     
+    const CutPoint *prev = &m_cpg->getCp (m_fn->getEntryBlock ());
+    for (const CutPoint *cp : m_cps)
+    {
+      m_states.push_back (m_states.back ());
+      SymStore &s = m_states.back ();
+      
+      const CpEdge *edg = m_cpg->getEdge (*prev, *cp);
+      assert (edg);
+      m_edges.push_back (edg);
+      sexec.execCpEdg (s, *edg, m_side);
+      
+      prev = cp;
+    }
+    
+    //for (Expr v : m_side) m_solver.assertExpr (v);
     
   }
 

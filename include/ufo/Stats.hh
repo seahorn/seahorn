282994e0c8839515d8c2d59b3474f55e38a386f6
Arie Gurfinkel <arie@cmu.edu>
2016-02-12 17:55:18 -0800

    [NEW] more timing functions

--- include/ufo/Stats.hh
+++ include/ufo/Stats.hh
@@ -176,6 +176,23 @@ namespace ufo
   //   auto_timer (const std::string &name) : n(name) { Stats::resume (n); }
   //   ~auto_timer () { Stats::stop (n); }
   // };
+
+  template <typename Output>
+  class TimeIt
+  {
+    const char* m_msg;
+    Output &m_out;
+    Stopwatch m_sw;
+    
+  public:
+    TimeIt (const char *msg, Output out) : m_msg (msg), m_out (out) {}
+    ~TimeIt () 
+    {
+      m_sw.stop ();
+      m_out << "TimeIt: " << m_msg << " " << m_sw << "\n";
+    }
+    
+  };
   
   class ScopedStats 
   {
@@ -196,6 +213,8 @@ namespace ufo
 
 }
 
+#define SEA_MEASURE_FN ufo::ScopedStats __stats__(__FUNCTION__)
+#define SEA_MEASURE_FN_LAST ufo::ScopedStats __stats_last__(__FUNCTION__, true)
 
 
 #endif

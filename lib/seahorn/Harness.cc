7fadf8428ad20004bd025aa786b281f2e32a39d8
Edward J. Schwartz <eschwartz@cert.org>
2016-05-04 14:37:09 -0400

    Cleanup some old comments and whitespace

--- lib/seahorn/Harness.cc
+++ lib/seahorn/Harness.cc
@@ -91,8 +91,8 @@ namespace seahorn
       Type *pRT = nullptr;
       if (RT->isIntegerTy ()) pRT = RT->getPointerTo ();
       else pRT = Type::getInt8PtrTy (getGlobalContext());
-      
-      
+
+
       ArrayType* AT = ArrayType::get(RT, values.size());
 
       // Convert Expr to LLVM constants
@@ -106,7 +106,7 @@ namespace seahorn
                                               true,
                                               GlobalValue::PrivateLinkage,
                                               ConstantArray::get(AT, LLVMarray));
-      
+
       // Build the body of the harness function
       BasicBlock *BB = BasicBlock::Create(getGlobalContext(), "entry", HF);
       IRBuilder<> Builder(BB);
@@ -119,8 +119,6 @@ namespace seahorn
                                                    ConstantInt::get(CountType, 0));
 
       Value *LoadCounter = Builder.CreateLoad(Counter);
-      //Value* Idx[] = {ConstantInt::get(CountType, 0), LoadCounter};
-      //Value *ArrayLookup = Builder.CreateLoad(Builder.CreateInBoundsGEP(CA, Idx));
 
       Value* Args[] = {LoadCounter,
                        Builder.CreateBitCast(CA, pRT),
@@ -137,15 +135,15 @@ namespace seahorn
         std::string RS;
         llvm::raw_string_ostream RSO(RS);
         RT->print(RSO);
-        
+
         name = Twine("__seahorn_get_value_").concat(RSO.str()).str();
       }
       else
         name = "__seahorn_get_value_ptr";
-      
+
       Constant *GetValue =
         Harness->getOrInsertFunction(name,
-                                     FunctionType::get(RT, makeArrayRef (ArgTypes, 3), 
+                                     FunctionType::get(RT, makeArrayRef (ArgTypes, 3),
                                                        false));
       assert(GetValue);
       Value* RetValue = Builder.CreateCall(GetValue, makeArrayRef(Args, 3));

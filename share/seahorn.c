4e2e567d7c44b266081487e98c083f6ceb112789
Arie Gurfinkel <arie@cmu.edu>
2016-06-14 22:35:44 -0400

    Dummy implementation of mem-wrap functions

--- share/seahorn.c
+++ share/seahorn.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
 void __VERIFIER_error() {
   printf("__VERIFIER_error was executed\n");
@@ -26,3 +27,19 @@ get_value_int(16)
 get_value_int(8)
 
 get_value_helper(intptr_t, ptr)
+
+/** Dummy implementation of memory wrapping functions */
+
+void __sea_mem_store (void *src, void *dst, size_t sz)
+{
+  /* if dst is a legal address */
+  memcpy (dst, src, sz);
+  /* else if dst is illegal, do nothing */
+}
+
+void __sea_mem_load (void *dst, void *src, size_t sz)
+{
+  /* if src is a legal address */
+  memcpy (dst, src, sz);
+  /* else, if src is illegal, return a dummy value */
+}

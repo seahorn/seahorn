9dcb8d4a7d9c7cfc46892bddbdeba2c59c235085
jorge <navasjorgea@gmail.com>
2016-06-29 18:53:14 -0700

    Added WTO of a graph: - Added a class that computes the weak topological order of a graph using BGL api. - Added a llvm pass that computes the wto of a function.

---
+++ include/seahorn/Analysis/WeakTopologicalOrderPass.hh
@@ -0,0 +1,44 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+
+#include "ufo/LlvmBgl.hpp" 
+#include "seahorn/Analysis/WeakTopologicalOrder.hh"
+
+using namespace llvm;
+
+namespace seahorn
+{
+
+  /// Construct weak topological order of a CFG of a function
+  class WeakTopologicalOrderPass : public llvm::FunctionPass
+  {
+    typedef WeakTopoOrder<llvm::Function> wto_t;
+
+    wto_t m_wto;
+
+  public:
+
+    typedef typename wto_t::const_iterator const_iterator;
+
+    static char ID;
+    
+    WeakTopologicalOrderPass () : FunctionPass (ID), m_wto() {}
+    
+    virtual void getAnalysisUsage (llvm::AnalysisUsage &AU) const;
+
+    virtual bool runOnFunction (llvm::Function &F); 
+
+    const_iterator begin () const {return m_wto.begin ();}
+    const_iterator end () const {return m_wto.end ();}
+   
+    // TODO: wto_t has more methods that should be exposed here,
+    // specially those to iterate over the nested components of a
+    // given basic block.
+
+    virtual const char* getPassName () const {return "WeakTopologicalOrder";}
+  };
+
+}
+
+
+

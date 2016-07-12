e7279a004ee28094312e8338ae860eade51b3b2b
jorge <navasjorgea@gmail.com>
2016-07-11 17:34:42 -0700

    Printed more stats

--- lib/Analysis/DSA/DsaInfo.cc
+++ lib/Analysis/DSA/DsaInfo.cc
@@ -4,6 +4,7 @@
 #include "llvm/Target/TargetLibraryInfo.h"
 #include "llvm/Analysis/MemoryBuiltins.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/CommandLine.h"
 
 #include "seahorn/Analysis/DSA/Info.hh"
 #include "seahorn/Analysis/DSA/Global.hh"
@@ -13,6 +14,12 @@
 
 using namespace seahorn::dsa;
 using namespace llvm;
+
+static llvm::cl::opt<bool>
+DsaInfoDetails("new-dsa-info-details",
+    llvm::cl::desc ("Print Dsa info details"),
+    llvm::cl::init (false),
+    llvm::cl::Hidden);
 
 void Info::getAnalysisUsage (AnalysisUsage &AU) const {
   AU.addRequired<DataLayoutPass> ();
@@ -55,9 +62,8 @@ void Info::countMemoryAccesses (Module &M)
   // A node can be read or modified even if there is no a memory
   // load/store for it. This can happen after nodes are unified.
   // 
-  // Here we just count the number of **non-trivial** memory
-  // loads/stores because it is useful for passes that will instrument
-  // them.
+  // Here we just count the number of **non-trivial** memory accesses
+  // because it is useful for passes that will instrument them.
   for (auto &F: M) {
     for (inst_iterator i = inst_begin(F), e = inst_end(F); i != e; ++i)  {
       const Instruction *I = &*i;
@@ -77,16 +83,18 @@ void Info::countMemoryAccesses (Module &M)
       
 void Info::printMemoryAccesses (NodeInfoVector &nodes, llvm::raw_ostream &o) 
 {
+  o << " --- Access information\n";
+
   unsigned int total_accesses = 0;
   for (auto &n: nodes) 
     total_accesses += n.getAccesses();
 
-  o << nodes.size ()  << " Total number of read and/or modified nodes.\n";      
-  o << total_accesses << " Total number of memory load/stores.\n";     
+  o << "\t" << nodes.size ()  << " number of read and modified nodes.\n";      
+  o << "\t" << total_accesses << " number of non-trivial memory accesess (load/store/memcpy/memset/memmove).\n";     
   
   //  --- Print a summary of accesses
   unsigned int summ_size = 5;
-  o << "Summary of the " << summ_size  << " nodes with more load/stores:\n";
+  o << "\t Summary of the " << summ_size  << " nodes with more accesses:\n";
   std::sort (nodes.begin (), nodes.end (),
              [](NodeInfo n1, NodeInfo n2)
              { return (n1.getAccesses() > n2.getAccesses());});
@@ -96,16 +104,16 @@ void Info::printMemoryAccesses (NodeInfoVector &nodes, llvm::raw_ostream &o)
       if (summ_size <= 0) break;
       summ_size--;
       if (n.getAccesses() == 0) break;
-      o << "  [Node Id " << n.getId()  << "] " 
+      o << "\t  [Node Id " << n.getId()  << "] " 
              << n.getAccesses() << " (" << (int) (n.getAccesses() * 100 / total_accesses) 
-             << "%) of total memory load/stores\n" ;
+             << "%) of total memory accesses\n" ;
     }
-    o << "  ...\n";
   }
 }
 
 void Info::printMemoryTypes (NodeInfoVector &nodes, llvm::raw_ostream& o) 
 {
+  o << " --- Type information\n";
   unsigned num_collapses = 0;
   unsigned num_typed_nodes = 0;
   for (auto &n: nodes) {
@@ -113,21 +121,47 @@ void Info::printMemoryTypes (NodeInfoVector &nodes, llvm::raw_ostream& o)
     num_typed_nodes += (std::distance(n.getNode()->types().begin(),
                                       n.getNode()->types().end()) > 0);
   }
-  o << num_typed_nodes << "  Total number of typed nodes.\n";
-  o << num_collapses << "  Total number of collapsed nodes.\n";
+  o << "\t" << num_typed_nodes << " number of typed nodes.\n";
+  o << "\t" << num_collapses << " number of collapsed nodes.\n";
 
   // TODO: print all node's types
 }
 
 void Info::printMemoryAllocSites (NodeInfoVector &nodes, llvm::raw_ostream& o) 
 {
+  o << " --- Allocation site information\n";
+
   // Assign a unique (deterministic) id to each allocation site
   unsigned id = 0;
-  typedef boost::container::flat_set<NodeInfo> NodeInfoSet;
+
   // map each allocation site to a set of nodes
+  typedef boost::container::flat_set<NodeInfo> NodeInfoSet;
   DenseMap<const llvm::Value*, std::pair<unsigned, NodeInfoSet> > alloc_map;
+
+  // number of nodes without allocation sites
+  unsigned num_orphan_nodes = 0;
+  // number of checks belonging to orphan nodes.
+  unsigned num_orphan_checks = 0;
+  // keep track of maximum number of allocation sites in a single node
+  unsigned max_alloc_sites = 0;
+
   // iterate over all nodes
   for (auto &n: nodes) {
+
+    unsigned num_alloc_sites = std::distance(n.getNode()->begin(), n.getNode()->end());
+    if (num_alloc_sites == 0) {
+      
+      // errs () << "Node without allocation site: ";
+      // n.getNode()->write(errs());
+      // errs () << "\n";
+
+      num_orphan_nodes++;
+      num_orphan_checks += n.getAccesses();
+      continue;
+    } else {
+      max_alloc_sites = std::max (max_alloc_sites, num_alloc_sites);
+    }
+
     // iterate over all allocation sites
     for (const llvm::Value*v : boost::make_iterator_range(n.getNode()->begin(),
                                                           n.getNode()->end())) {
@@ -142,13 +176,20 @@ void Info::printMemoryAllocSites (NodeInfoVector &nodes, llvm::raw_ostream& o)
     }
   }
 
-  o << alloc_map.size() << "  Total number of allocation sites\n";
+  o << "\t" << alloc_map.size()  << " number of allocation sites\n";
+  o << "\t   " << max_alloc_sites  << " max number of allocation sites in a node\n";
+  o << "\t" << num_orphan_nodes  << " number of nodes without allocation site\n";
+  o << "\t" << num_orphan_checks << " number of memory accesses without allocation site\n";
+
+  if (!DsaInfoDetails) return;
+  
+  // --- print for each allocation site the set of nodes
   // TODO: print allocation sites ordered by id
   for (auto &kv: alloc_map) {
     o << "  [Alloc site Id " << kv.second.first << " DSNode Ids {";
     bool first = true;
     for (typename NodeInfoSet::iterator it = kv.second.second.begin(),
-             et = kv.second.second.end(); it != et; ) {
+               et = kv.second.second.end(); it != et; ) {
       if (!first) o << ",";
       else first = false;
       o << it->getId ();
@@ -156,7 +197,16 @@ void Info::printMemoryAllocSites (NodeInfoVector &nodes, llvm::raw_ostream& o)
     }
     o << "}]  " << *(kv.first) << "\n";
   }
-
+  
+  // --- print for each node its set of allocation sites
+  for (auto &n: nodes) {
+      o << "  [Node Id " << n.getId () << " Allocation sites Ids {";
+      for (const llvm::Value*v : boost::make_iterator_range(n.getNode()->begin(),
+                                                            n.getNode()->end())) {
+        o << alloc_map[v].first << " ";
+      }
+      o << "}]\n"; 
+  }
 }
 
 bool Info::runOnModule (Module &M) {
@@ -166,9 +216,8 @@ bool Info::runOnModule (Module &M) {
   m_g = &getAnalysis<Global>().getGraph();
  
   // -- Assign a unique (deterministic) id to each node
-  //    Important: the order of [ m_g->begin()...m_g->end() ) must be
-  //    deterministic. Otherwise, we will assign node id's that will
-  //    change from one execution to another.
+  //    FIXME: [ m_g->begin()...m_g->end() ) can change from one
+  //    execution to another
   unsigned node_id = 0; 
   for (auto const& n: boost::make_iterator_range(m_g->begin(), m_g->end()))
     m_nodes_map.insert(std::make_pair(&n, NodeInfo(&n, ++node_id)));
@@ -199,4 +248,4 @@ bool Info::runOnModule (Module &M) {
 char Info::ID = 0;
 
 static llvm::RegisterPass<Info> 
-X ("dsa-info", "Collect stats and information for dsa clients");
+X ("new-dsa-info", "Collect stats and information for dsa clients");

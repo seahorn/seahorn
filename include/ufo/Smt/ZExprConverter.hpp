799042591b87ba655d7a8ad91440daa396a45e89
Arie Gurfinkel <arie@cmu.edu>
2016-08-01 17:30:09 -0400

    [FIX] order of high and low in bv::extract

--- include/ufo/Smt/ZExprConverter.hpp
+++ include/ufo/Smt/ZExprConverter.hpp
@@ -445,6 +445,7 @@ namespace ufo
       else if (isOpX<BEXTRACT> (e))
       {
         z3::ast a (ctx, marshal (bv::earg (e), ctx, cache, seen));
+        assert (bv:high (e) < bv::low (e));
         res = Z3_mk_extract (ctx, bv::high (e), bv::low (e), a);
       }
       else if (isOpX<AND> (e) || isOpX<OR> (e) ||

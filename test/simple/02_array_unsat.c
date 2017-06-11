2aedca843f9ee6ec2b6c1dabbeba26cf0ab61a55
caballa <navasjorgea@gmail.com>
2017-06-11 20:48:58 +0200

    [FIXED] 02_array_sat.c was actually safe (unsat) but we were getting sat (unsafe) because seaopt was removing the assertion.

--- test/simple/02_array_unsat.c
+++ test/simple/02_array_unsat.c
@@ -1,20 +1,17 @@
 // RUN: %sea pf -O0 "%s"  2>&1 | OutputCheck %s
-// CHECK: ^sat$
+// CHECK: ^unsat$
 
 #include "seahorn/seahorn.h"
 #define N 10
 
 int a[N];
-extern int nd ();
 
 int main ()
 {
   int i;
 
-  for (i=0;i<N;i++) {
-    if (nd())
-      a[i] = 0;
-  }
+  for (i=0;i<N;i++)
+    a[i] = 0;
 
   for (i=0;i<N;i++)
     sassert (a[i] == 0);

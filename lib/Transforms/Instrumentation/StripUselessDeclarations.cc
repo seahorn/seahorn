377d1dccf4377a36cd5935670b021d96af7d62dc
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2017-10-19 14:14:00 -0400

    Optionally keep declarations of known library functions
    Options to prevent `StripUselessDeclarations` from removing known
    library functions.

--- lib/Transforms/Instrumentation/StripUselessDeclarations.cc
+++ lib/Transforms/Instrumentation/StripUselessDeclarations.cc
@@ -1,3 +1,12 @@
+/**
+SeaHorn Verification Framework
+Copyright (c) 2017 Arie Gurfinkel and Jorge Navas.
+All Rights Reserved.
+
+Released under a modified BSD license, please see license.txt for full
+terms.
+*/
+
 #include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
 #include "llvm/Transforms/Utils/Local.h"
@@ -6,45 +15,69 @@
 #include "llvm/IR/IRBuilder.h"
 
 #include "llvm/Support/raw_ostream.h"
-  
+#include "llvm/Analysis/TargetLibraryInfo.h"
+
 using namespace llvm;
+
+static cl::opt<bool>
+KeepLibFn("keep-lib-fn",
+          cl::desc("Do not strip external references to well-known library functions"),
+          cl::init(false));
 
 namespace
 {
   class StripUselessDeclarations : public ModulePass
   {
     unsigned m_count;
+    TargetLibraryInfo *m_tli;
+
   public:
     static char ID;
-    StripUselessDeclarations () : ModulePass (ID), m_count(0) {}
+    StripUselessDeclarations () : ModulePass (ID), m_count(0), m_tli(nullptr) {}
 
     void getAnalysisUsage (AnalysisUsage &AU) const override
-    {AU.setPreservesAll ();}
+    {
+      AU.addRequired<TargetLibraryInfoWrapperPass> ();
+      AU.setPreservesAll ();
+    }
 
+    bool isUseless (Function &F) {
+        auto name = F.getName ();
+        if (name.startswith ("llvm.")) return false;
+        if (name.startswith ("malloc") ||
+            name.startswith ("calloc") ||
+            name.startswith ("memset") ||
+            name.startswith ("memcpy")) return false;
+
+        if (name.startswith ("klee_")) return false;
+
+        if (name.startswith ("seahorn.") ||
+            name.startswith ("__seahorn") ||
+            name.startswith ("verifier.")) return false;
+
+        if (name.startswith ("__VERIFIER")) return false;
+
+        if (name.startswith ("__builtin")) return false;
+
+        if (KeepLibFn) {
+            if (!m_tli)
+                m_tli = &getAnalysis<TargetLibraryInfoWrapperPass> ().getTLI();
+
+            // known library function
+            LibFunc::Func libfn;
+            if (m_tli->getLibFunc (name, libfn)) return false;
+        }
+
+        return true;
+
+    }
     bool runOnModule (Module &M) override
     {
       for (auto &F : M)
       {
         if (F.isDeclaration ())
         {
-          auto name = F.getName ();
-          if (name.startswith ("llvm.")) continue;
-          if (name.startswith ("malloc") ||
-              name.startswith ("calloc") ||
-              name.startswith ("memset") ||
-              name.startswith ("memcpy")) continue;
-
-          if (name.startswith ("klee_")) continue;
-
-          if (name.startswith ("seahorn.") ||
-	      name.startswith ("__seahorn") ||
-              name.startswith ("verifier.")) continue;
-
-          if (name.startswith ("__VERIFIER")) continue;
-
-          if (name.startswith ("__builtin")) continue;
-        
-          strip (F);
+            if (isUseless (F)) strip (F);
         }
         else
           stripAsm (F);
@@ -55,7 +88,7 @@ namespace
     void strip (Function &F)
     {
       SmallVector<Value*, 16> args;
-      
+
       Value::use_iterator UI = F.use_begin (), E = F.use_end ();
       for (; UI != E;)
       {
@@ -71,14 +104,14 @@ namespace
           // -- do not delete functions that take no arguments,
           // -- they are treated as non-deterministic anyhow
           if (CS.arg_empty ()) continue;
-          
+
           if (!F.getReturnType ()->isVoidTy ())
           {
             // insert call to nondet fn
             std::string newName ("verifier.nondet.stripextern.");
             newName += F.getName ();
             newName += ".";
-            
+
             Function &ndfn = seahorn::createNewNondetFn (*F.getParent (),
                                                          *F.getReturnType (), 1,
                                                          newName);
@@ -86,11 +119,11 @@ namespace
             Builder.SetInsertPoint (CS.getInstruction ());
             CallInst *call = Builder.CreateCall (&ndfn);
             call->setDebugLoc (CS.getInstruction ()->getDebugLoc ());
-            
+
             // -- replace old call with nondet one
             CS.getInstruction ()->replaceAllUsesWith (call);
           }
-          
+
           // -- delete the old call instruction
           if (FU->use_empty ())
           {
@@ -103,7 +136,7 @@ namespace
         }
       }
     }
-    
+
     void stripAsm (Function &F)
     {
       SmallVector<CallInst*, 8> dead;
@@ -120,12 +153,12 @@ namespace
             }
           }
         }
-        
+
         for (auto *call : dead)
         {
           CallSite CS(call);
           args.insert (args.end (), CS.arg_begin (), CS.arg_end ());
-          
+
           if (!call->use_empty ())
           {
             std::string fName = "nondet.asm.";
@@ -139,7 +172,7 @@ namespace
             nCall->setDebugLoc (call->getDebugLoc ());
             call->replaceAllUsesWith (nCall);
           }
-          
+
           call->eraseFromParent ();
           for (auto &a : args)
             seahorn::RecursivelyDeleteTriviallyDeadInstructions (a);
@@ -148,10 +181,10 @@ namespace
         dead.clear ();
       }
     }
-    
+
   };
   char StripUselessDeclarations::ID = 0;
-  
+
 }
 
 namespace seahorn
@@ -162,5 +195,3 @@ namespace seahorn
 
 static llvm::RegisterPass<StripUselessDeclarations> X ("strip-useless-decls",
                                                        "Replace declarations by nondet");
-
-

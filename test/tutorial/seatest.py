a6fc41d73c6a3116023bca9a4b7641dbd3091f55
Charles Lei <mrthefakeperson@gmail.com>
2018-03-20 00:13:51 -0400

    update test target; tutorial tests for optimization levels

--- test/tutorial/seatest.py
+++ test/tutorial/seatest.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python
 
-from sys import argv, maxsize
+from sys import argv, maxsize, exit
 from subprocess import Popen, PIPE
 
 if len(argv) < 3:
     exit(1)
 harness_opt, exe_opt, code, pf_args = argv[1], argv[2], argv[3], argv[4:]
 
-output, _ = Popen(["sea", "pf", "--cex=%s" % harness_opt, code] + pf_args, stdout=PIPE).communicate()
+m32_or_m64 = "-m64" if maxsize > 2**32 else "-m32"
+output, _ = Popen(["sea", "pf", "--cex=%s" % harness_opt, m32_or_m64, code] + pf_args, stdout=PIPE).communicate()
 
 if "sat" in output.split("\n"):
     print("sat")
-    m32_or_m64 = "-m64" if maxsize > 2**32 else "-m32"
     Popen(["sea", "exe", m32_or_m64, "-g", code, harness_opt, "-o", exe_opt]).communicate()
-    expected_error, _ = Popen("%s" % exe_opt, stdout=PIPE).communicate()
+    expected_error, _ = Popen(exe_opt, stdout=PIPE).communicate()
     print(expected_error)
 elif "unsat" in output.split("\n"):
     print("unsat")
 else:
     print(output)
-    exit(1)
--- test/tutorial/ssft_simple_3.c
+++ test/tutorial/ssft_simple_3.c
@@ -1,5 +1,12 @@
-// RUN: %shorntest %t-harness.ll %t-debug %s | OutputCheck %s
+// RUN: %shorntest %t-harness.ll %t-debug %s --max-depth=15 --enable-indvar | OutputCheck %s
+// RUN: %shorntest %t-harness0.ll %t-debug0 %s --max-depth=15 -O0 --enable-indvar | OutputCheck %s --check-prefix=XFAIL_UNKNOWN
+// RUN: %shorntest %t-harness1.ll %t-debug1 %s --max-depth=15 -O1 --enable-indvar | OutputCheck %s
+// RUN: %shorntest %t-harness2.ll %t-debug2 %s --max-depth=15 -O2 --enable-indvar | OutputCheck %s
+// RUN: %shorntest %t-harness3.ll %t-debug3 %s --max-depth=15 -O3 --enable-indvar | OutputCheck %s
+// RUN: %shorntest %t-harness-.ll %t-debug- %s --max-depth=15 | OutputCheck %s --check-prefix=XFAIL_NOT_UNKNOWN
 // CHECK: ^unsat$
+// XFAIL_UNKNOWN: ^unknown$
+// XFAIL_NOT_UNKNOWN: ^unsat$
 
 /* requires --enable-indvar */
 int unknown(void);

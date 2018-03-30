fbcc24789c14268fc394c303fa019d2081280f52
caballa <navasjorgea@gmail.com>
2018-03-30 11:25:49 -0700

    FIX Test has unterminated run lines (with '\')

--- test/solve/03_unsat.c
+++ test/solve/03_unsat.c
@@ -1,8 +1,8 @@
-// RUN: %sea --mem=-1 -m64 pf --step=large -g --horn-global-constraints=true --track=mem \
-             --horn-stats --enable-nondet-init --strip-extern --externalize-addr-taken-functions \
-             --horn-singleton-aliases=true --devirt-functions --horn-ignore-calloc=false \
-             --enable-indvar --enable-loop-idiom --horn-make-undef-warning-error=false \
-             --max-depth=35 --inline "%s"
+// RUN: %sea --mem=-1 -m64 pf --step=large -g --horn-global-constraints=true --track=mem 
+//             --horn-stats --enable-nondet-init --strip-extern --externalize-addr-taken-functions 
+//             --horn-singleton-aliases=true --devirt-functions --horn-ignore-calloc=false 
+//             --enable-indvar --enable-loop-idiom --horn-make-undef-warning-error=false 
+//             --max-depth=35 --inline "%s"
 // CHECK: ^unsat$
 
 // This sometimes gets stuck in the solver on level 34. Disable untill we move to

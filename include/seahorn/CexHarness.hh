e59328eea5ae8d8388531910875d1bf24c24b2df
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2018-08-23 17:01:37 -0400

    Formatting

--- include/seahorn/CexHarness.hh
+++ include/seahorn/CexHarness.hh
@@ -1,59 +1,57 @@
 #ifndef _CEXHARNESS__HH_
 #define _CEXHARNESS__HH_
 
-#include "llvm/IR/Module.h"
-#include "llvm/IR/DataLayout.h"
-#include "llvm/Analysis/TargetLibraryInfo.h"
 #include "seahorn/Bmc.hh"
 #include "seahorn/MemSimulator.hh"
+#include "llvm/Analysis/TargetLibraryInfo.h"
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/Module.h"
 
-namespace seahorn
-{
-  using namespace llvm;
+namespace seahorn {
+using namespace llvm;
 
-  // Wrapper for BmcTrace and MemSimulator objects.
-  // 
-  // Both are defined wrt to the same BmcTrace but they can have
-  // different models associated (accessed via the virtual method
-  // eval).
-  class BmcTraceWrapper {
-    BmcTrace &m_trace;
-  public:
-    BmcTraceWrapper(BmcTrace &trace):
-      m_trace(trace) {}
+// Wrapper for BmcTrace and MemSimulator objects.
+//
+// Both are defined wrt to the same BmcTrace but they can have
+// different models associated (accessed via the virtual method
+// eval).
+class BmcTraceWrapper {
+  BmcTrace &m_trace;
 
-    /// access to expression factory
-    ExprFactory &efac () { return m_trace.engine().efac(); }
-    
-    /// The number of basic blocks in the trace 
-    virtual unsigned size () const
-    { return m_trace.size(); }
-    
-    /// The basic block at a given location 
-    virtual const llvm::BasicBlock* bb (unsigned loc) const
-    { return m_trace.bb(loc); }
+public:
+  BmcTraceWrapper(BmcTrace &trace) : m_trace(trace) {}
 
-    /// The value of the instruction at the given location     
-    virtual Expr eval (unsigned loc, const llvm::Instruction &inst, bool complete);
-    virtual Expr eval (unsigned loc, Expr v, bool complete);    
-  };
+  /// access to expression factory
+  ExprFactory &efac() { return m_trace.engine().efac(); }
 
-  class BmcTraceMemSim: public BmcTraceWrapper {
-    MemSimulator &m_mem_sim;
-  public:
-    
-    BmcTraceMemSim(MemSimulator &mem_sim):
-      BmcTraceWrapper(mem_sim.trace()),
-      m_mem_sim(mem_sim) {}
+  /// The number of basic blocks in the trace
+  virtual unsigned size() const { return m_trace.size(); }
 
-    virtual Expr eval (unsigned loc, const llvm::Instruction &inst, bool complete) override;
-    virtual Expr eval (unsigned loc, Expr v, bool complete) override;
-  };
+  /// The basic block at a given location
+  virtual const llvm::BasicBlock *bb(unsigned loc) const {
+    return m_trace.bb(loc);
+  }
 
-  
-  std::unique_ptr<llvm::Module> createCexHarness (BmcTraceWrapper &trace, 
-						  const DataLayout &dl,
-						  const TargetLibraryInfo &tli);
-}
+  /// The value of the instruction at the given location
+  virtual Expr eval(unsigned loc, const llvm::Instruction &inst, bool complete);
+  virtual Expr eval(unsigned loc, Expr v, bool complete);
+};
+
+class BmcTraceMemSim : public BmcTraceWrapper {
+  MemSimulator &m_mem_sim;
+
+public:
+  BmcTraceMemSim(MemSimulator &mem_sim)
+      : BmcTraceWrapper(mem_sim.trace()), m_mem_sim(mem_sim) {}
+
+  virtual Expr eval(unsigned loc, const llvm::Instruction &inst,
+                    bool complete) override;
+  virtual Expr eval(unsigned loc, Expr v, bool complete) override;
+};
+
+std::unique_ptr<llvm::Module> createCexHarness(BmcTraceWrapper &trace,
+                                               const DataLayout &dl,
+                                               const TargetLibraryInfo &tli);
+} // namespace seahorn
 
 #endif

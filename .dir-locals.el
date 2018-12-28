efb9daafc26a6e31d217c3cc698a31bd6d6ac359
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2018-12-28 12:14:17 -0500

    dir-locals (for emacs)

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,5 +5,7 @@
  (c++-mode (c-basic-offset . 2))
  (c-mode . ((c-basic-offset . 2)))
  (nil . ((eval . (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode)))
+         (eval . (if (boundp 'c-offsets-alist)
+                     (add-to-list 'c-offsets-alist '(innamespace . -))))
          ))
  )

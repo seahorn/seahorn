e8a77fe2e9c3f6400c5768ec776f11671f000255
Jakub Kuderski <kubakuderski@gmail.com>
2019-01-07 13:05:23 -0500

    Enable bmc tests. Don't log bmc.

--- test/bmc/test-bmc-diamond-1.true.c
+++ test/bmc/test-bmc-diamond-1.true.c
@@ -1,6 +1,6 @@
-// RUN: %sea bpf -O0 --bmc=mono --bound=10  --horn-stats --inline --log=bmc "%s" 2>&1 | OutputCheck %s
-// RUN: %sea bpf -O0 --horn-bmc-crab=false  --bmc=path --bound=10  --horn-stats --inline --log=bmc "%s" 2>&1 | OutputCheck %s
-// RUN: %sea bpf -O0 --horn-bmc-crab=true  --bmc=path --bound=10  --horn-stats --inline --log=bmc "%s" 2>&1 | OutputCheck %s
+// RUN: %sea bpf -O0 --bmc=mono --bound=10  --horn-stats --inline "%s" 2>&1 | OutputCheck %s
+// RUN: %sea bpf -O0 --horn-bmc-crab=false  --bmc=path --bound=10  --horn-stats --inline "%s" 2>&1 | OutputCheck %s
+// RUN: %sea bpf -O0 --horn-bmc-crab=true  --bmc=path --bound=10  --horn-stats --inline "%s" 2>&1 | OutputCheck %s
 // CHECK: ^unsat$
 
 /** 

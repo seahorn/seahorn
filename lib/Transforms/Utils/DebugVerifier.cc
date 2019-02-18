7dfd25b33f067b232b0673b7766a97cf02562a91
Jakub Kuderski <kubakuderski@gmail.com>
2019-02-18 18:00:51 -0500

    Print time in DebugVerifier. Allow to skip verification.

--- lib/Transforms/Utils/DebugVerifier.cc
+++ lib/Transforms/Utils/DebugVerifier.cc
@@ -7,9 +7,16 @@
 #include "seahorn/Support/SeaDebug.h"
 #include "seahorn/Support/SeaLog.hh"
 
+#include <chrono>
+#include <ctime>
 #include <string>
 
 #define DV_LOG(...) LOG("debug-verifier", __VA_ARGS__)
+
+static llvm::cl::opt<bool>
+    NoVerify("sea-verifier-noop",
+             llvm::cl::desc("Don't verify in DebugVerifier (for debugging)"),
+             llvm::cl::init(false));
 
 using namespace llvm;
 
@@ -35,13 +42,14 @@ public:
 char DebugVerifierPass::ID = 0;
 
 bool DebugVerifierPass::runOnModule(Module &M) {
-  DV_LOG(errs() << "\n~~~ Running seahorn::DebugVerifierPass "
-                   "for "
-                << m_passName << " (" << m_instanceID
-                << ") ~~~~ \n");
+  DV_LOG(errs() << "\n~~~ Running seahorn::DebugVerifierPass for " << m_passName
+                << " (" << m_instanceID << ") ~~~~ \n");
+  DV_LOG(const auto now = std::chrono::system_clock::now();
+         std::time_t time = std::chrono::system_clock::to_time_t(now);
+         errs() << "\tTime: " << std::ctime(&time) << "\n");
 
   bool brokenDebugInfo = false;
-  if (llvm::verifyModule(M, &(errs()), &brokenDebugInfo)) {
+  if (!NoVerify && llvm::verifyModule(M, &(errs()), &brokenDebugInfo)) {
     ERR << "Module verification failed!\n";
     llvm_unreachable("Terminating after failed module verification");
   }

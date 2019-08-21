98a70cf617b52c7729e6f2e7b0877ff383c97774
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2019-08-21 19:11:24 -0400

    Port handling n-ary bvadd to new expression library

--- include/seahorn/Expr/Smt/ZExprConverter.hh
+++ include/seahorn/Expr/Smt/ZExprConverter.hh
@@ -399,7 +399,7 @@ template <typename M> struct BasicExprMarshal {
     } else if (isOpX<AND>(e) || isOpX<OR>(e) || isOpX<ITE>(e) ||
                isOpX<XOR>(e) || isOpX<PLUS>(e) || isOpX<MINUS>(e) ||
                isOpX<MULT>(e) || isOpX<STORE>(e) || isOpX<ARRAY_MAP>(e) ||
-               isOpX<BCONCAT>(e)) {
+               isOpX<BCONCAT>(e) || isOpX<BADD>(e)) {
       std::vector<z3::ast> pinned;
       std::vector<Z3_ast> args;
 
@@ -430,11 +430,20 @@ template <typename M> struct BasicExprMarshal {
         Z3_func_decl fdecl = reinterpret_cast<Z3_func_decl>(args[0]);
         res = Z3_mk_map(ctx, fdecl, e->arity() - 1, &args[1]);
       } else if (isOp<BCONCAT>(e)) {
+        // convert n-ary bv-concat to binary since Z3 has no API to create it
         assert(args.size() > 2);
         res = args.back();
         for (unsigned sz = args.size(), i = sz - 2; i < sz; --i) {
           res = Z3_mk_concat(ctx, args[i], res);
           assert(res && "Creating concat failed");
+        }
+      }
+      else if (isOp<BADD>(e)) {
+        // convert n-ary bv-add to binary since Z3 has no API to create it
+        assert(args.size() > 2);
+        res = args.back();
+        for (unsigned sz = args.size(), i = sz - 2; i < sz; --i) {
+          res = Z3_mk_bvadd(ctx, args[i], res);
         }
       }
     } else

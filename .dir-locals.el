14e1cd181e16f8ce8966217caf7ef7842e8cf63f
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2019-10-08 09:55:35 -0400

    dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -7,5 +7,6 @@
  (nil . ((eval . (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode)))
          (eval . (if (boundp 'c-offsets-alist)
                      (add-to-list 'c-offsets-alist '(innamespace . -))))
+         (eval . (setq lsp-file-watch-threshold nil))
          ))
  )

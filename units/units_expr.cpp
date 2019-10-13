4f244d97a1a47c488221f2154a286e2bbd65d013
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2019-10-12 22:24:05 -0400

    [expr] simple C++ wrapper over gmp for mpz_class
    We use very little of mpz_class functionality from gmp.
    However, it prevents us from compiling with assertions disabled.
    
    Started on our own implementation that hopefully provides all that we really
    need.

--- units/units_expr.cpp
+++ units/units_expr.cpp
@@ -1,20 +1,40 @@
 #include "seahorn/Expr/Expr.hh"
+#include "seahorn/Expr/ExprGmp.hh"
 #include "seahorn/Expr/ExprLlvm.hh"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/ADT/APInt.h"
 #include "boost/lexical_cast.hpp"
 #include "doctest.h"
 
+inline expr::mpz_class toMpzE(const llvm::APInt &v) {
+  // Based on:
+  // https://llvm.org/svn/llvm-project/polly/trunk/lib/Support/GICHelper.cpp
+  // return v.getSExtValue ();
+
+  llvm::APInt abs;
+  abs = v.isNegative() ? v.abs() : v;
+
+  const uint64_t *rawdata = abs.getRawData();
+  unsigned numWords = abs.getNumWords();
+
+  // TODO: Check if this is true for all platforms.
+  expr::mpz_class res;
+  mpz_import(res.get_mpz_t(), numWords, -1, sizeof(uint64_t), 0, 0, rawdata);
+
+  if (v.isNegative()) res.neg(); 
+  return res;
+}
+
 TEST_CASE("expr.APInt") {
   using namespace expr;
   using namespace llvm;
 
   APInt numA(64, "30903631872", 10);
-  mpz_class numZ = toMpz(numA);
+  expr::mpz_class numZ = toMpzE(numA);
   errs() << "numA: " << numA << "\n";
-  errs() << "numZ: " << boost::lexical_cast<std::string>(numZ) << "\n";
+  errs() << "numZ: " << numZ.to_string() << "\n";
 
-  CHECK(numA.toString(10, false) == boost::lexical_cast<std::string>(numZ));
+  CHECK(numA.toString(10, false) == numZ.to_string());
 
 }
 
@@ -23,9 +43,9 @@ TEST_CASE("expr.APInt.large") {
   using namespace llvm;
 
   APInt numA2(85, "453350497004842588831744", 10);
-  mpz_class numZ2 = toMpz(numA2);
+  expr::mpz_class numZ2 = toMpzE(numA2);
   errs() << "numA2: " << numA2 << "\n";
-  errs() << "numZ2: " << boost::lexical_cast<std::string>(numZ2) << "\n";
+  errs() << "numZ2: " << numZ2.to_string() << "\n";
 
-  CHECK(numA2.toString(10, false) == boost::lexical_cast<std::string>(numZ2));
+  CHECK(numA2.toString(10, false) == numZ2.to_string());
 }

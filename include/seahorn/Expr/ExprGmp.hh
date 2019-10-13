5e9283a14f866b9eb008426d78c31d76f35958d4
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2019-10-12 22:24:05 -0400

    [expr] simple C++ wrapper over gmp for mpz_class
    We use very little of mpz_class functionality from gmp.
    However, it prevents us from compiling with assertions disabled.
    
    Started on our own implementation that hopefully provides all that we really
    need.

---
+++ include/seahorn/Expr/ExprGmp.hh
@@ -0,0 +1,66 @@
+/// A simple interface to gmp
+#pragma once
+#include <algorithm>
+#include <cstring>
+#include <gmp.h>
+#include <string>
+
+namespace expr {
+
+namespace {
+extern "C" {
+typedef void (*__gmp_freefunc_t)(void *, size_t);
+}
+struct scoped_cstring {
+  char *m_str;
+  scoped_cstring(char *s) { m_str = s; }
+  ~scoped_cstring() {
+    __gmp_freefunc_t freefunc;
+    mp_get_memory_functions(nullptr, nullptr, &freefunc);
+    (*freefunc)(m_str, std::strlen(m_str) + 1);
+  }
+};
+} // namespace
+class mpz_class {
+  mpz_t m_num;
+
+public:
+  mpz_class() { mpz_init(m_num); }
+  ~mpz_class() { mpz_clear(m_num); }
+
+  mpz_class(const mpz_class &v) { mpz_init_set(m_num, v.m_num); }
+  mpz_class(mpz_class &&v) {
+    *m_num = *v.m_num;
+    mpz_init(v.m_num);
+  }
+
+  mpz_class(unsigned long v) { mpz_init_set_ui(m_num, v); }
+  mpz_class(signed long v) { mpz_init_set_si(m_num, v); }
+
+  mpz_class &operator=(const mpz_class &v) {
+    mpz_set(m_num, v.m_num);
+    return *this;
+  }
+  mpz_class &operator=(mpz_class &&v) {
+    swap(v);
+    return *this;
+  }
+
+  mpz_class &neg() {
+    mpz_neg(m_num, m_num);
+    return *this;
+  }
+
+  void swap(mpz_class &v) { std::swap(*m_num, *v.m_num); }
+
+  mpz_srcptr get_mpz_t() const { return m_num; }
+  mpz_ptr get_mpz_t() { return m_num; }
+
+  signed long int get_si() const { return mpz_get_si(m_num); }
+  unsigned long int get_ui() const { return mpz_get_ui(m_num); }
+  std::string to_string(unsigned base = 10) {
+    scoped_cstring res(mpz_get_str(0, base, m_num));
+    return std::string(res.m_str);
+  }
+};
+} // namespace expr

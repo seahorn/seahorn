c761e39d5e442f69dd477643b8d6636aa4994b98
caballa <navasjorgea@gmail.com>
2020-01-23 15:17:10 -0800

    Moved caching of bounce functions into the particular resolver.
    This is in preparation for supporting sea-dsa as a resolver for
    indirect calls.  The code was assuming that a bounce function can be
    reused if its type signature matches the callsite. With sea-dsa we
    want to reuse a bounce function if they have the same type signature
    and they cover the same set of targets.

--- include/seahorn/Transforms/Utils/DevirtFunctions.hh
+++ include/seahorn/Transforms/Utils/DevirtFunctions.hh
@@ -29,7 +29,7 @@ AliasSetId typeAliasId(const llvm::Function &F);
 AliasSetId typeAliasId(llvm::CallSite &CS);
 } // end namespace devirt_impl
 
-enum CallSiteResolverKind { RESOLVER_TYPES, RESOLVER_CHA };
+enum class CallSiteResolverKind { RESOLVER_TYPES, RESOLVER_CHA };
 
 /*
  * Generic class API for resolving indirect calls
@@ -41,6 +41,7 @@ protected:
   CallSiteResolver(CallSiteResolverKind kind) : m_kind(kind) {}
 
 public:
+  using AliasSetId = devirt_impl::AliasSetId;    
   using AliasSet = llvm::SmallVector<const llvm::Function *, 16>;
 
   virtual ~CallSiteResolver() {}
@@ -49,6 +50,10 @@ public:
 
   /* out contains all the callees to resolve CS */
   virtual void getTargets(llvm::CallSite &CS, AliasSet &out) = 0;
+
+  /* for reusing bounce functions */
+  virtual llvm::Function* getBounceFunction(llvm::CallSite &CS) = 0;
+  virtual void cacheBounceFunction(llvm::CallSite &CS, llvm::Function* bounce) = 0;
 };
 
 // forward declaration
@@ -60,21 +65,31 @@ class ClassHierarchyAnalysis;
  */
 class CallSiteResolverByTypes final : public CallSiteResolver {
 public:
+  using AliasSetId = CallSiteResolver::AliasSetId;  
+  using AliasSet = CallSiteResolver::AliasSet;
+  
   CallSiteResolverByTypes(llvm::Module &M);
 
-  ~CallSiteResolverByTypes();
+  virtual ~CallSiteResolverByTypes();
 
   void getTargets(llvm::CallSite &CS, AliasSet &out);
 
+  llvm::Function* getBounceFunction(llvm::CallSite& CS);
+  
+  void cacheBounceFunction(llvm::CallSite& CS, llvm::Function* bounce);
+  
 private:
-  using AliasSetId = devirt_impl::AliasSetId;
-  using TypeAliasSetMap = llvm::DenseMap<AliasSetId, AliasSet>;
 
+  using TargetsMap = llvm::DenseMap<AliasSetId, AliasSet>;
+  using BounceMap = llvm::DenseMap<AliasSetId, llvm::Function *>;
+  
   // -- the module
   llvm::Module &m_M;
-  // -- map from alias-id to the corresponding alias set
-  TypeAliasSetMap m_typeAliasSets;
-
+  // -- map from alias-id to the corresponding targets
+  TargetsMap m_targets_map;
+  // -- map alias set id to an existing bounce function
+  BounceMap m_bounce_map;
+  
   void populateTypeAliasSets(void);
 };
 
@@ -83,12 +98,19 @@ private:
  */
 class CallSiteResolverByCHA final : public CallSiteResolver {
 public:
+  using AliasSetId = CallSiteResolver::AliasSetId;  
+  using AliasSet = CallSiteResolver::AliasSet;
+  
   CallSiteResolverByCHA(llvm::Module &M);
 
   ~CallSiteResolverByCHA();
 
   void getTargets(llvm::CallSite &CS, AliasSet &out);
 
+  llvm::Function* getBounceFunction(llvm::CallSite& CS);
+  
+  void cacheBounceFunction(llvm::CallSite& CS, llvm::Function* bounce);
+  
 private:
   std::unique_ptr<ClassHierarchyAnalysis> m_cha;
 };
@@ -116,24 +138,24 @@ private:
   // Worklist of call sites to transform
   llvm::SmallVector<llvm::Instruction *, 32> m_worklist;
 
+  // allow creating of indirect calls during devirtualization
+  // (required for soundness)
+  bool m_allowIndirectCalls;
+  
   /// turn the indirect call-site into a direct one
-  void mkDirectCall(llvm::CallSite CS, CallSiteResolver *CSR,
-                    bool AllowIndirectCalls);
+  void mkDirectCall(llvm::CallSite CS, CallSiteResolver *CSR);
 
   /// create a bounce function that calls functions directly
-  llvm::Function *mkBounceFn(llvm::CallSite &CS, CallSiteResolver *CSR,
-                             bool AllowIndirectCalls);
+  llvm::Function *mkBounceFn(llvm::CallSite &CS, CallSiteResolver *CSR);
 
 public:
-  DevirtualizeFunctions(llvm::CallGraph *cg);
+  DevirtualizeFunctions(llvm::CallGraph *cg, bool allowIndirectCalls);
 
+  ~DevirtualizeFunctions();
+  
   // Resolve all indirect calls in the Module using a particular
   // callsite resolver.
-  bool resolveCallSites(llvm::Module &M, CallSiteResolver *CSR,
-                        /* allow creating of indirect calls during
-                           devirtualization (required for
-                           soundness) */
-                        bool AllowIndirectCalls = false);
+  bool resolveCallSites(llvm::Module &M, CallSiteResolver *CSR);
 
   // -- VISITOR IMPLEMENTATION --
 

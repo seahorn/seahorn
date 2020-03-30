c528f260c2f8a3cdcffe447e3001157d0109bce2
Isabel Garcia <isabel.garcia@imdea.org>
2020-03-30 17:58:20 +0200

    Added basic map operations using lambda terms

--- include/seahorn/Expr/ExprOpFiniteMap.hh
+++ include/seahorn/Expr/ExprOpFiniteMap.hh
@@ -1,10 +1,11 @@
 /// Finite Maps
 #pragma once
+#include "seahorn/Expr/Expr.hh"
 #include "seahorn/Expr/ExprApi.hh"
 #include "seahorn/Expr/ExprCore.hh"
+#include "seahorn/Expr/ExprOpBinder.hh"
 #include "seahorn/Expr/ExprOpBool.hh"
 #include "seahorn/Expr/ExprOpCore.hh"
-#include "seahorn/Expr/Expr.hh"
 
 namespace expr {
 
@@ -32,6 +33,54 @@ inline Expr get(Expr a, Expr idx) { return expr::mk<GET>(a, idx); }
 inline Expr set(Expr a, Expr idx, Expr v) { return expr::mk<SET>(a, idx, v); }
 // inline Expr set(Expr a, const ExprVector& keys, Expr idx, Expr v) { return
 // mk<GET>(a, keys, idx, v); }
+
+// creates a set of keys as a lambda function
+Expr make_lambda_map_keys(ExprVector keys, ExprFactory &efac) {
+
+  Expr x = bind::intConst(mkTerm<std::string>("x", efac));
+
+  Expr lmd_bot = bind::abs<LAMBDA>(std::array<Expr, 1>{x},
+                                   mkTerm<expr::mpz_class>(0, efac));
+  // up to here, it will be the same for all keysets
+
+  int count = 1;
+
+  Expr lmd_tmp = lmd_bot;
+
+  for (auto key : keys) {
+    Expr nA = mkTerm<expr::mpz_class>(count, efac);
+    Expr cmp = mk<EQ>(key, x);
+    Expr ite = boolop::lite(cmp, nA, op::bind::betaReduce(lmd_tmp, x));
+    lmd_tmp = bind::abs<LAMBDA>(std::array<Expr, 1>{x}, ite);
+    count++;
+  }
+
+  return lmd_tmp;
+}
+
+Expr new_map_lambda(ExprFactory &efac) {
+  return mkTerm<expr::mpz_class>(0, efac);
+  // TODO: change by the same as unitialized memory!!
+}
+Expr get_map_lambda(Expr map, Expr keys, Expr key) {
+
+    Expr pos_in_map = op::bind::betaReduce(keys, key);
+
+    return op::bind::betaReduce(map, pos_in_map);
+  }
+
+  Expr set_map_lambda(Expr map, Expr keys, Expr key, Expr value,
+                      ExprFactory & efac) {
+
+    Expr x = bind::intConst(mkTerm<std::string>("x", efac));
+
+    Expr pos_in_map = op::bind::betaReduce(keys, key);
+    Expr cmp = mk<EQ>(x, pos_in_map);
+    Expr ite = boolop::lite(cmp, value, op::bind::betaReduce(map, pos_in_map));
+    Expr new_map = bind::abs<LAMBDA>(std::array<Expr, 1>{x}, ite);
+
+    return new_map;
+  }
 
 } // namespace finite_map
 } // namespace op
--- units/units_finite_maps.cpp
+++ units/units_finite_maps.cpp
@@ -19,10 +19,7 @@ TEST_CASE("expr.finite_map.create_map") {
   Expr map = finite_map::constFiniteMap(keys);
 
   CHECK(map);
-
   CHECK(boost::lexical_cast<std::string>(*map) == "defk(k1)");
-
-  errs() << *map << "\n";
 }
 
 TEST_CASE("expr.finite_map.set") {
@@ -43,8 +40,6 @@ TEST_CASE("expr.finite_map.set") {
 
   CHECK(map1);
   CHECK(boost::lexical_cast<std::string>(*map1) == "set(defk(k1), k1, 30)");
-
-  errs() << *map1 << "\n";
 }
 
 TEST_CASE("expr.finite_map.get") {
@@ -63,8 +58,6 @@ TEST_CASE("expr.finite_map.get") {
 
   CHECK(eget);
   CHECK(boost::lexical_cast<std::string>(*eget) == "get(defk(k1), k1)");
-
-  errs() << *eget << "\n";
 }
 
 TEST_CASE("expr.finite_map.create_set_3") {
@@ -91,21 +84,114 @@ TEST_CASE("expr.finite_map.create_set_3") {
   CHECK(boost::lexical_cast<std::string>(*map) ==
         "set(defk(k1, k2, k3), k1, 31)");
 
-  errs() << *map << "\n";
-
   v = mkTerm<expr::mpz_class>(32, efac);
   map = finite_map::set(map, key2, v);
   CHECK(map);
   CHECK(boost::lexical_cast<std::string>(*map) ==
         "set(set(defk(k1, k2, k3), k1, 31), k2, 32)");
 
-  errs() << *map << "\n";
-
   v = mkTerm<expr::mpz_class>(33, efac);
   map = finite_map::set(map, key3, v);
   CHECK(map);
   CHECK(boost::lexical_cast<std::string>(*map) ==
         "set(set(set(defk(k1, k2, k3), k1, 31), k2, 32), k3, 33)");
+}
 
-  errs() << *map << "\n";
+TEST_CASE("expr.finite_map.create_keys_lambda") {
+  using namespace std;
+  using namespace expr;
+  using namespace expr::op;
+  using namespace seahorn;
+
+  ExprFactory efac;
+
+  Expr x = bind::intConst(mkTerm<string>("x", efac));
+
+  ExprVector keys;
+
+  keys.push_back(bind::intConst(mkTerm<string>("k1", efac)));
+
+  Expr lambda_keys = finite_map::make_lambda_map_keys(keys, efac);
+  CHECK(boost::lexical_cast<std::string>(lambda_keys) ==
+        "(lambda (INT) ite(k1=(B0:INT), 1, 0))");
+
+  keys.push_back(bind::intConst(mkTerm<std::string>("k2", efac)));
+  lambda_keys = finite_map::make_lambda_map_keys(keys, efac);
+
+  CHECK(boost::lexical_cast<std::string>(lambda_keys) ==
+        "(lambda (INT) ite(k2=(B0:INT), 2, ite(k1=(B0:INT), 1, 0)))");
+
+  keys.push_back(bind::intConst(mkTerm<std::string>("k3", efac)));
+  lambda_keys = finite_map::make_lambda_map_keys(keys, efac);
+
+  CHECK(boost::lexical_cast<std::string>(lambda_keys) ==
+        "(lambda (INT) ite(k3=(B0:INT), 3, ite(k2=(B0:INT), 2, "
+        "ite(k1=(B0:INT), 1, 0))))");
+}
+
+TEST_CASE("expr.finite_map.set_map_lambda") {
+  using namespace std;
+  using namespace expr;
+  using namespace expr::op;
+  using namespace seahorn;
+
+  ExprFactory efac;
+
+  ExprVector keys;
+
+  Expr k1 = bind::intConst(mkTerm<std::string>("k1", efac));
+  keys.push_back(k1);
+  keys.push_back(bind::intConst(mkTerm<std::string>("k2", efac)));
+  keys.push_back(bind::intConst(mkTerm<std::string>("k3", efac)));
+
+  Expr lambda_keys = finite_map::make_lambda_map_keys(keys, efac);
+  CHECK(boost::lexical_cast<std::string>(lambda_keys) ==
+        "(lambda (INT) ite(k3=(B0:INT), 3, ite(k2=(B0:INT), 2, "
+        "ite(k1=(B0:INT), 1, 0))))");
+
+  Expr value = mkTerm<expr::mpz_class>(42, efac);
+
+  Expr map = finite_map::new_map_lambda(efac);
+  // set the value of k1
+  map = finite_map::set_map_lambda(map, lambda_keys, k1, value, efac);
+
+  CHECK(boost::lexical_cast<std::string>(map) ==
+        "(lambda (INT) ite((B0:INT)=ite(k3=k1, 3, ite(k2=k1, 2, ite(k1=k1, 1, "
+        "0))), 42, 0))");
+}
+
+TEST_CASE("expr.finite_map.get_after_set_map_lambda") {
+  using namespace std;
+  using namespace expr;
+  using namespace expr::op;
+  using namespace seahorn;
+
+  ExprFactory efac;
+
+  ExprVector keys;
+
+  Expr k1 = bind::intConst(mkTerm<std::string>("k1", efac));
+  keys.push_back(k1);
+  keys.push_back(bind::intConst(mkTerm<std::string>("k2", efac)));
+  keys.push_back(bind::intConst(mkTerm<std::string>("k3", efac)));
+
+  Expr lambda_keys = finite_map::make_lambda_map_keys(keys, efac);
+  CHECK(boost::lexical_cast<std::string>(lambda_keys) ==
+        "(lambda (INT) ite(k3=(B0:INT), 3, ite(k2=(B0:INT), 2, "
+        "ite(k1=(B0:INT), 1, 0))))");
+
+  Expr value = mkTerm<expr::mpz_class>(42, efac);
+
+  Expr map = finite_map::new_map_lambda(efac); // init map
+  map = finite_map::set_map_lambda(map, lambda_keys, k1, value, efac);
+
+  CHECK(boost::lexical_cast<std::string>(map) ==
+        "(lambda (INT) ite((B0:INT)=ite(k3=k1, 3, ite(k2=k1, 2, ite(k1=k1, 1, "
+        "0))), 42, 0))");
+
+  Expr get_value = finite_map::get_map_lambda(map, lambda_keys, k1);
+
+  CHECK(boost::lexical_cast<std::string>(get_value) ==
+        "ite(ite(k3=k1, 3, ite(k2=k1, 2, ite(k1=k1, 1, 0)))=ite(k3=k1, 3, "
+        "ite(k2=k1, 2, ite(k1=k1, 1, 0))), 42, 0)");
 }

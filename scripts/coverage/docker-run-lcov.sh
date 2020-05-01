dd62adbbb888a531b4db5bdb675eb60fe0bd5c02
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-05-01 17:04:24 -0400

    [scripts] coverage computation under docker

---
+++ scripts/coverage/docker-run-lcov.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+##
+## Run tests and collect coverage
+##
+## The script expects to run in a docker container with fixed paths
+##
+
+# run tests
+# XXX limit to tests that succeed currently, re-enable when all test pass
+# lit /seahorn/test
+lit /seahorn/test/opsem
+lit /seahorn/test/mcfuzz
+lit /seahorn/test/simple
+
+# collect coverage
+lcov -c -d /seahorn/build/lib/ \
+     -b /seahorn/build/ -o coverage.info \
+     --gcov-tool=/seahorn/scripts/coverage/llvm-gcov-10.sh
+
+# filter seahorn relevant info 
+lcov -e coverage.info '/seahorn/*' -o all.info
---
+++ scripts/coverage/llvm-gcov-10.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+exec llvm-cov-10 gcov "$@"

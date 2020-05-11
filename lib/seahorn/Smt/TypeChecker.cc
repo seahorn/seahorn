ea875331f5d5651e7cb6448af7be1d6acc691dbe
colinxm <colin1@cool1.ca>
2020-05-11 20:49:08 +0000

    Extended TypeChecker to support boolean expressions

--- lib/seahorn/Smt/TypeChecker.cc
+++ lib/seahorn/Smt/TypeChecker.cc
@@ -12,10 +12,80 @@ namespace {
 
 //==-- main implementation goes here --==//
 class TCVR {
+  ExprVector expTypeStack;
+
+  //TODO
+  bool isInt (Expr exp, bool & properTypes) {
+    if (bind::isIntConst(exp)) {
+      return true;
+    }
+    return false;
+  }
+
+  bool isBool (Expr exp, bool & properTypes) {
+    properTypes = true;
+    Expr boolSort = sort::boolTy(exp->efac());
+
+    if (isOpX <AND> (exp) || isOpX <OR> (exp) || isOpX <XOR> (exp)) {
+       int numChildren = exp->arity();
+
+      for (int i = 0; i < numChildren; i ++) {
+        properTypes = properTypes && expTypeStack.back () == boolSort;
+        expTypeStack.pop_back();
+      }
+
+      return true;
+    }
+    else if (isOpX <IMPL> (exp) || isOpX <IFF> (exp)) {
+      properTypes = expTypeStack.back () == boolSort;
+      expTypeStack.pop_back();
+      properTypes = properTypes && expTypeStack.back () == boolSort;
+      expTypeStack.pop_back();
+
+      return true;
+    }
+    else if (isOpX <NEG> (exp)) {
+      properTypes = expTypeStack.back() == boolSort;
+      expTypeStack.pop_back();
+
+      return true;
+    }
+    else if (isOpX<TRUE>(exp) || isOpX<FALSE>(exp) || bind::isBoolVar(exp) || bind::isBoolConst(exp)) {
+      return true;
+    }
+
+    return false;
+  }
 
   /// Called after children have been visited
   Expr postVisit(Expr exp) {
     LOG("tc", llvm::errs() << "post visiting expression: " << *exp << "\n";);
+    Expr type = nullptr;
+
+    bool properTypes = true;
+
+    if (isBool(exp, properTypes)) {
+      type = sort::boolTy(exp->efac());
+    }
+    else if (isInt(exp, properTypes)) {
+      type = sort::intTy(exp->efac());
+    }
+    else {
+      LOG("tc", llvm::errs() << "unknown type" << *exp << "\n");
+      type = Expr();
+    }
+
+    if (!properTypes){
+      LOG("tc", llvm::errs() << "improper types at:" << *exp << "\n";);
+      type = Expr();
+      expTypeStack.push_back(type);
+      return type;
+    }
+
+  //  if (type) {
+      expTypeStack.push_back(type);
+  // }
+
     return exp;
   }
 
@@ -30,8 +100,11 @@ public:
 
   Expr knownTypeOf(Expr e) {
     LOG("tc", llvm::errs() << "Request type of: " << *e << "\n";);
-    // if e is non null, return ANY type, ow return nil expression
-    return e ? sort::anyTy(e->efac()) : Expr();
+
+    Expr type = expTypeStack.back();
+    expTypeStack.clear();
+
+    return type;
   }
 };
 
@@ -42,8 +115,9 @@ class TCV : public std::unary_function<Expr, VisitAction> {
 public:
   TCV() : m_rw(std::make_shared<TCVR>()) {}
   VisitAction operator()(Expr exp) {
-    if (m_rw->preVisit(exp))
+    if (m_rw->preVisit(exp)) {
       return VisitAction::changeDoKidsRewrite(exp, m_rw);
+    }
     else
       return VisitAction::skipKids();
   }

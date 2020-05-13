e1937f2c7f1fe84b85e36fe7d70f5ee780e38955
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-05-13 11:37:21 -0400

    fix: Pass -log option to SeaDsa as well as to SeaHorn

--- lib/Support/SeaDebug.cc
+++ lib/Support/SeaDebug.cc
@@ -26,18 +26,26 @@ void seahorn::SeaEnableLog(std::string x) {
   seadsa::SeaDsaEnableLog(x);
 }
 
+namespace seadsa {
+void SeaDsaEnableLog(std::string x);
+}
+
 namespace seahorn {
 struct LogOpt {
-  void operator=(const std::string &tag) const { seahorn::SeaEnableLog(tag); }
+  void operator=(const std::string &tag) const {
+    seahorn::SeaEnableLog(tag);
+    seadsa::SeaDsaEnableLog(tag);
+  }
 };
 
 LogOpt loc;
-}
+} // namespace seahorn
 
 static llvm::cl::opt<seahorn::LogOpt, true, llvm::cl::parser<std::string>>
     LogClOption("log", llvm::cl::desc("Enable specified log level"),
-                llvm::cl::location(seahorn::loc), llvm::cl::value_desc("string"),
-                llvm::cl::ValueRequired, llvm::cl::ZeroOrMore);
+                llvm::cl::location(seahorn::loc),
+                llvm::cl::value_desc("string"), llvm::cl::ValueRequired,
+                llvm::cl::ZeroOrMore);
 
 #else
 #endif

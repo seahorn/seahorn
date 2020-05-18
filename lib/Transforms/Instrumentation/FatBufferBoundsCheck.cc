cc964666f49bf87e137db4df961c4406d048684e
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-05-17 23:26:38 -0400

    refactor(fat-bnd-check): Cleanup code and refactor functionality
    Intention is to keep functionality the same. This code has not been tested.

--- lib/Transforms/Instrumentation/FatBufferBoundsCheck.cc
+++ lib/Transforms/Instrumentation/FatBufferBoundsCheck.cc
@@ -84,23 +84,19 @@ private:
   BuilderTy *Builder;
   Instruction *Inst;
   BasicBlock *ErrorBB;
-  Type *IntptrTy;
-  LLVMContext *Ctx;
-  Module *Mod;
+  Type *IntPtrTy;
   Function *m_setFatSlot0;   // set ptr base
   Function *m_setFatSlot1;   // set ptr size
   Function *m_getFatSlot0;   // get ptr base
   Function *m_getFatSlot1;   // get ptr size
   Function *m_copyFatSlots;  // copy ptr info
   Function *m_recoverFatPtr; // NEW: for ptr embed method only
-  Value *OrigPtr;
 
   BasicBlock *getErrorBB();
   void emitBranchToTrap(Value *Cmp = nullptr);
-  bool instrument(Value *Ptr, Value *Val, const DataLayout &DL);
-  bool instrumentAddress(Value *Ptr, const DataLayout &DL,
-                         Value *BasePtr = nullptr);
-  // Value* memToShadow(Value *mem, BuilderTy *builder);
+  bool instrument(Value *Ptr, Value *Val, const DataLayout &DL, Value *&RawPtr);
+  bool instrumentAlloca(AllocaInst *Ptr, const DataLayout &DL);
+  bool instrumentGep(GetElementPtrInst *Ptr);
 };
 } // namespace
 
@@ -127,7 +123,8 @@ BasicBlock *FatBufferBoundsCheck::getErrorBB() {
   // XXX solution is to unify how seahorn-specific functions are accessed to
   // XXX ensure that they are always created uniformly with the right
   // XXX attributes.
-  auto errorFn = Md->getOrInsertFunction("__VERIFIER_error", as, Type::getVoidTy(ctx));
+  auto errorFn =
+      Md->getOrInsertFunction("__VERIFIER_error", as, Type::getVoidTy(ctx));
   CallInst *TrapCall = Builder->CreateCall(errorFn);
   TrapCall->setDoesNotReturn();
   TrapCall->setDoesNotThrow();
@@ -168,13 +165,17 @@ void FatBufferBoundsCheck::emitBranchToTrap(Value *Cmp) {
 /// size of memory block that is touched.
 /// Returns true if any change was made to the IR, false otherwise.
 bool FatBufferBoundsCheck::instrument(Value *Ptr, Value *InstVal,
-                                      const DataLayout &DL) {
+                                      const DataLayout &DL, Value *&RawPtr) {
   uint64_t NeededSize = DL.getTypeStoreSize(InstVal->getType());
-  Value *NeededSizeVal = ConstantInt::get(IntptrTy, NeededSize);
+  Value *NeededSizeVal = ConstantInt::get(IntPtrTy, NeededSize);
   LOG("fat-bnd-check", errs() << "Instrument " << *Ptr << " for "
                               << Twine(NeededSize) << " bytes\n");
   SizeOffsetEvalType SizeOffset = ObjSizeEval->compute(Ptr);
   Value *Or;
+
+  Value *RecovPtr = Builder->CreateCall(
+      m_recoverFatPtr, Builder->CreateBitCast(Ptr, Builder->getInt8PtrTy()));
+  RawPtr = Builder->CreateBitCast(RecovPtr, Ptr->getType());
 
   if (!ObjSizeEval->bothKnown(SizeOffset)) {
     LOG("fat-bnd-check", errs() << "fatptr instrument " << *Ptr << " for "
@@ -189,23 +190,17 @@ bool FatBufferBoundsCheck::instrument(Value *Ptr, Value *InstVal,
       is_overflow := ptr_end < access_end
       is_access_bad := is_underflow or is_overflow
     */
-    // get start and end by calling internalized functions
     Value *Start = Builder->CreateCall(
         m_getFatSlot0, Builder->CreateBitCast(Ptr, Builder->getInt8PtrTy()));
     Value *Size = Builder->CreateCall(
         m_getFatSlot1, Builder->CreateBitCast(Ptr, Builder->getInt8PtrTy()));
-    Value *recov = Builder->CreateCall(
-        m_recoverFatPtr, Builder->CreateBitCast(Ptr, Builder->getInt8PtrTy()));
-    OrigPtr = Builder->CreateBitCast(recov, Ptr->getType());
-    Value *PtrInt = Builder->CreatePtrToInt(OrigPtr, IntptrTy);
+    Value *PtrAsInt = Builder->CreatePtrToInt(RawPtr, IntPtrTy);
     // Ptr >= Start
-    Value *CmpUnderFlow = Builder->CreateICmpULT(
-        PtrInt, Builder->CreateIntCast(Start, IntptrTy, true));
+    Value *CmpUnderFlow = Builder->CreateICmpULT(PtrAsInt, Start);
     // Start + Size >= Ptr + NeededSize
-    Value *accessEnd = Builder->CreateAdd(PtrInt, NeededSizeVal);
-    Value *ptrEnd = Builder->CreateAdd(Start, Size);
-    Value *CmpOverFlow = Builder->CreateICmpULT(
-        Builder->CreateIntCast(ptrEnd, IntptrTy, true), accessEnd);
+    Value *AccessEnd = Builder->CreateAdd(PtrAsInt, NeededSizeVal);
+    Value *PtrEnd = Builder->CreateAdd(Start, Size);
+    Value *CmpOverFlow = Builder->CreateICmpULT(PtrEnd, AccessEnd);
     Or = Builder->CreateOr(CmpUnderFlow, CmpOverFlow);
   } else {
     // size and offest statically computed
@@ -228,19 +223,17 @@ bool FatBufferBoundsCheck::instrument(Value *Ptr, Value *InstVal,
     Or = Builder->CreateOr(Cmp2, Cmp3);
     if (!SizeCI || SizeCI->getValue().slt(0)) {
       Value *Cmp1 =
-          Builder->CreateICmpSLT(Offset, ConstantInt::get(IntptrTy, 0));
+          Builder->CreateICmpSLT(Offset, ConstantInt::get(IntPtrTy, 0));
       Or = Builder->CreateOr(Cmp1, Or);
     }
-    OrigPtr = Ptr;
   }
   emitBranchToTrap(Or);
 
   return true;
 }
 
-/* Record information of address Ptr, store/update the base address and size */
-bool FatBufferBoundsCheck::instrumentAddress(Value *Ptr, const DataLayout &DL,
-                                             Value *BasePtr) {
+bool FatBufferBoundsCheck::instrumentAlloca(AllocaInst *Ptr,
+                                            const DataLayout &DL) {
   /**
      Rewrite pointer creation to embed base address and size in the extended
      part of the pointer.
@@ -253,82 +246,80 @@ bool FatBufferBoundsCheck::instrumentAddress(Value *Ptr, const DataLayout &DL,
      pointer goes. Replaces old pointer by new everywhere. Then plug the old
      pointer into the placeholder left for it.
    */
-  Ptr->setName("raw_ptr");
-  Type *resultType;
+  auto *AllocedTy = Ptr->getAllocatedType();
   /* BasePtr not provided: processing store instructions
     with_base := call set_fat_slot0(ptr)
     with_size_and_base := call set_fat_slot1(with_base)
     replace_all(ptr, with_size_and_base)
   */
-  if (!BasePtr) {
-    if (auto *ALI = dyn_cast<AllocaInst>(Ptr)) {
-      resultType = ALI->getAllocatedType();
-    } else {
-      ERR << "Unexpected instruction: " << *Ptr << "\n";
-      assert(false && "Unexpected instruction");
-    }
-    CallInst *withBase = Builder->CreateCall(
-        m_setFatSlot0, {Constant::getNullValue(Builder->getInt8PtrTy()),
-                        ConstantInt::get(IntptrTy, 0)});
-    // set_fat_slot1(Ptr, Size)
-    CallInst *withSize = Builder->CreateCall(
-        m_setFatSlot1, {Constant::getNullValue(Builder->getInt8PtrTy()),
-                        ConstantInt::get(IntptrTy, 0)});
-    Value *casted =
-        Builder->CreateBitCast(withSize, resultType->getPointerTo());
-    Ptr->replaceAllUsesWith(casted);
-    Value *sizeVal;
-    // alloca
-    if (auto *ALI = dyn_cast<AllocaInst>(Ptr)) {
-      uint64_t size = DL.getTypeStoreSize(ALI->getAllocatedType());
-      sizeVal = ConstantInt::get(IntptrTy, size);
-    } else {
-      ERR << "Could not determine base address: " << *Ptr;
-      assert(false && "unexpected lack of base address");
-    }
-    // set_fat_slot0(Ptr, Base)
-    Builder->SetInsertPoint(withBase);
-    Value *argA = Builder->CreateBitCast(Ptr, Builder->getInt8PtrTy());
-    Value *argB = Builder->CreatePtrToInt(Ptr, IntptrTy);
-    withBase->setArgOperand(0, argA);
-    withBase->setArgOperand(1, argB);
-    Builder->SetInsertPoint(withSize);
-    argA = Builder->CreateBitCast(withBase, Builder->getInt8PtrTy());
-    withSize->setArgOperand(0, argA);
-    withSize->setArgOperand(1, sizeVal);
-  } else {
-    /* Additional BasePtr is provided: processing GEP
-      copied := call copy_fat_slots(ptr, base_ptr)
-      replace_all(ptr, copied)
-    */
-    // copy_fat_slots(Ptr, BasePtr)
-    if (auto *GEP = dyn_cast<GetElementPtrInst>(Ptr)) {
-      resultType = GEP->getResultElementType();
-    } else {
-      ERR << "Unexpected non-gep instruction: " << *Ptr << "\n";
-      assert(false && "only handling GEP instructions");
-    }
-    CallInst *copied = Builder->CreateCall(
-        m_copyFatSlots, {Constant::getNullValue(Builder->getInt8PtrTy()),
-                         Constant::getNullValue(Builder->getInt8PtrTy())});
-    Value *casted = Builder->CreateBitCast(copied, resultType->getPointerTo());
-    LOG("fat-bnd-check", errs() << "casting  " << *Ptr << " to " << *casted
-                                << " with type " << *resultType->getPointerTo()
-                                << " \n";);
-    Ptr->replaceAllUsesWith(casted);
-    Builder->SetInsertPoint(copied);
-    copied->setArgOperand(0,
-                          Builder->CreateBitCast(Ptr, Builder->getInt8PtrTy()));
-    copied->setArgOperand(
-        1, Builder->CreateBitCast(BasePtr, Builder->getInt8PtrTy()));
-  }
+
+  // -- forward created calls. Arguments will be filled later
+  // -- create a call to set slot0
+  CallInst *withBase = Builder->CreateCall(
+      m_setFatSlot0, {Constant::getNullValue(Builder->getInt8PtrTy()),
+                      ConstantInt::get(IntPtrTy, 0)});
+  // -- create a call to set slot1
+  CallInst *withSize = Builder->CreateCall(
+      m_setFatSlot1, {Constant::getNullValue(Builder->getInt8PtrTy()),
+                      ConstantInt::get(IntPtrTy, 0)});
+  // -- cast result of setting slot1 to same type as returned by alloca
+  Value *NewPtr = Builder->CreateBitCast(withSize, AllocedTy->getPointerTo());
+  // -- replace all uses of the pointer with fat pointer
+  // -- later pass will ensure that the pointer is recovered if it is used to
+  // access memory
+  Ptr->replaceAllUsesWith(NewPtr);
+
+  // set_fat_slot0(Ptr, Base)
+  Builder->SetInsertPoint(withBase);
+  Value *argA = Builder->CreateBitCast(Ptr, Builder->getInt8PtrTy());
+  Value *argB = Builder->CreatePtrToInt(Ptr, IntPtrTy);
+  withBase->setArgOperand(0, argA);
+  withBase->setArgOperand(1, argB);
+
+  // set_fat_slot1(Ptr, Size)
+  argA = Builder->CreateBitCast(withBase, Builder->getInt8PtrTy());
+  withSize->setArgOperand(0, argA);
+  Builder->SetInsertPoint(withSize);
+  auto size = DL.getTypeStoreSize(AllocedTy);
+  auto *sizeVal = ConstantInt::get(IntPtrTy, size);
+  withSize->setArgOperand(1, sizeVal);
 
   return true;
 }
 
+
+bool FatBufferBoundsCheck::instrumentGep(GetElementPtrInst *Ptr) {
+  /*
+    Transformation:
+     copied := call copy_fat_slots(ptr, base_ptr)
+     replace_all(ptr, copied)
+ */
+  // copy_fat_slots(Ptr, BasePtr)
+  auto GepTy = Ptr->getResultElementType();
+  auto *BasePtr = Ptr->getPointerOperand();
+
+  CallInst *SlotCopyCall = Builder->CreateCall(
+      m_copyFatSlots, {Constant::getNullValue(Builder->getInt8PtrTy()),
+                       Constant::getNullValue(Builder->getInt8PtrTy())});
+  Value *Casted = Builder->CreateBitCast(SlotCopyCall, GepTy->getPointerTo());
+  Ptr->replaceAllUsesWith(Casted);
+  LOG("fat-bnd-check", errs()
+                           << "casting  " << *Ptr << " to " << *Casted
+                           << " with type " << *GepTy->getPointerTo() << "\n";);
+
+  Builder->SetInsertPoint(SlotCopyCall);
+  SlotCopyCall->setArgOperand(
+      0, Builder->CreateBitCast(Ptr, Builder->getInt8PtrTy()));
+  SlotCopyCall->setArgOperand(
+      1, Builder->CreateBitCast(BasePtr, Builder->getInt8PtrTy()));
+  return true;
+}
+
 bool FatBufferBoundsCheck::runOnFunction(Function &F) {
-  Mod = F.getParent();
-  const DataLayout &DL = F.getParent()->getDataLayout();
+  auto *M = F.getParent();
+  auto &C = F.getContext();
+
+  const auto &DL = M->getDataLayout();
   TLI = &getAnalysis<TargetLibraryInfoWrapperPass>().getTLI(F);
 
   ErrorBB = nullptr;
@@ -339,15 +330,12 @@ bool FatBufferBoundsCheck::runOnFunction(Function &F) {
   ObjectSizeOffsetEvaluator TheObjSizeEval(DL, TLI, F.getContext(), EvalOpts);
   ObjSizeEval = &TheObjSizeEval;
 
-  Ctx = &(F.getContext());
-  int LongSize = DL.getPointerSizeInBits();
-  IntptrTy = Type::getIntNTy(F.getContext(), LongSize);
+  IntPtrTy = DL.getIntPtrType(C);
 
-  // XXX: technically we should no be creating functions in a non-module pass
-  // XXX: is it to create / lookup them on every function?
+
   m_getFatSlot0 =
-      cast<Function>(Mod->getOrInsertFunction(SEA_GET_FAT_SLOT0, IntptrTy,
-                                              Type::getInt8PtrTy(*Ctx, 0))
+      cast<Function>(M->getOrInsertFunction(SEA_GET_FAT_SLOT0, IntPtrTy,
+                                            Type::getInt8PtrTy(C, 0))
                          .getCallee());
 
   m_getFatSlot0->setDoesNotThrow();
@@ -355,33 +343,32 @@ bool FatBufferBoundsCheck::runOnFunction(Function &F) {
   m_getFatSlot0->addParamAttr(0, Attribute::NoCapture);
 
   m_getFatSlot1 =
-      cast<Function>(Mod->getOrInsertFunction(SEA_GET_FAT_SLOT1, IntptrTy,
-                                              Type::getInt8PtrTy(*Ctx, 0))
+      cast<Function>(M->getOrInsertFunction(SEA_GET_FAT_SLOT1, IntPtrTy,
+                                            Type::getInt8PtrTy(C, 0))
                          .getCallee());
   m_getFatSlot1->setDoesNotThrow();
   m_getFatSlot1->setDoesNotReadMemory();
   m_getFatSlot1->addParamAttr(0, Attribute::NoCapture);
 
   m_setFatSlot0 = cast<Function>(
-      Mod->getOrInsertFunction(SEA_SET_FAT_SLOT0, Type::getInt8PtrTy(*Ctx, 0),
-                               Type::getInt8PtrTy(*Ctx, 0), IntptrTy)
+      M->getOrInsertFunction(SEA_SET_FAT_SLOT0, Type::getInt8PtrTy(C, 0),
+                             Type::getInt8PtrTy(C, 0), IntPtrTy)
           .getCallee());
   m_setFatSlot0->setDoesNotThrow();
   m_setFatSlot0->setDoesNotReadMemory();
   m_setFatSlot0->addParamAttr(0, Attribute::Returned);
 
   m_setFatSlot1 = cast<Function>(
-      Mod->getOrInsertFunction(SEA_SET_FAT_SLOT1, Type::getInt8PtrTy(*Ctx, 0),
-                               Type::getInt8PtrTy(*Ctx, 0), IntptrTy)
+      M->getOrInsertFunction(SEA_SET_FAT_SLOT1, Type::getInt8PtrTy(C, 0),
+                             Type::getInt8PtrTy(C, 0), IntPtrTy)
           .getCallee());
   m_setFatSlot1->setDoesNotThrow();
   m_setFatSlot1->setDoesNotReadMemory();
   m_setFatSlot1->addParamAttr(0, Attribute::Returned);
 
   m_copyFatSlots = cast<Function>(
-      Mod->getOrInsertFunction(SEA_COPY_FAT_SLOTS, Type::getInt8PtrTy(*Ctx, 0),
-                               Type::getInt8PtrTy(*Ctx, 0),
-                               Type::getInt8PtrTy(*Ctx, 0))
+      M->getOrInsertFunction(SEA_COPY_FAT_SLOTS, Type::getInt8PtrTy(C, 0),
+                             Type::getInt8PtrTy(C, 0), Type::getInt8PtrTy(C, 0))
           .getCallee());
   m_copyFatSlots->setDoesNotThrow();
   m_copyFatSlots->setDoesNotReadMemory();
@@ -389,8 +376,8 @@ bool FatBufferBoundsCheck::runOnFunction(Function &F) {
   m_copyFatSlots->addParamAttr(1, Attribute::NoCapture);
 
   m_recoverFatPtr = cast<Function>(
-      Mod->getOrInsertFunction(SEA_RECOVER_FAT_PTR, Type::getInt8PtrTy(*Ctx, 0),
-                               Type::getInt8PtrTy(*Ctx, 0))
+      M->getOrInsertFunction(SEA_RECOVER_FAT_PTR, Type::getInt8PtrTy(C, 0),
+                             Type::getInt8PtrTy(C, 0))
           .getCallee());
   m_recoverFatPtr->setDoesNotThrow();
   m_recoverFatPtr->setDoesNotReadMemory();
@@ -399,20 +386,19 @@ bool FatBufferBoundsCheck::runOnFunction(Function &F) {
   // check HANDLE_MEMORY_INST in include/llvm/Instruction.def for memory
   // touching instructions
   std::vector<Instruction *> AccessWorkList;
-  std::vector<Instruction *> AllocaList; // new register is created for addr
-  std::vector<Instruction *> GEPList;    // new register is created for addr
-  for (inst_iterator i = inst_begin(F), e = inst_end(F); i != e; ++i) {
+  std::vector<AllocaInst *> AllocaList; // new register is created for addr
+  std::vector<GetElementPtrInst *> GEPList;    // new register is created for addr
+  for (auto i = inst_begin(F), e = inst_end(F); i != e; ++i) {
     Instruction *I = &*i;
     if (isa<LoadInst>(I) || isa<StoreInst>(I) || isa<AtomicCmpXchgInst>(I) ||
         isa<AtomicRMWInst>(I)) {
       AccessWorkList.push_back(I);
-      continue;
     }
-    if (isa<AllocaInst>(I)) {
-      AllocaList.push_back(I);
+    else if (isa<AllocaInst>(I)) {
+      AllocaList.push_back(cast<AllocaInst>(I));
     }
-    if (isa<GetElementPtrInst>(I)) {
-      GEPList.push_back(I);
+    else if (isa<GetElementPtrInst>(I)) {
+      GEPList.push_back(cast<GetElementPtrInst>(I));
     }
   }
 
@@ -422,10 +408,7 @@ bool FatBufferBoundsCheck::runOnFunction(Function &F) {
     Builder->SetInsertPoint(Inst->getNextNode()); // insert after
     if (AllocaInst *ALI = dyn_cast<AllocaInst>(Inst)) {
       Type *allocTy = ALI->getAllocatedType();
-      if (allocTy->isArrayTy() || allocTy->isPointerTy() ||
-          allocTy->isStructTy()) {
-        MadeChange |= instrumentAddress(ALI, DL);
-      }
+      MadeChange |= instrumentAlloca(ALI, DL);
     } else {
       llvm_unreachable("unknown Instruction type");
     }
@@ -435,7 +418,7 @@ bool FatBufferBoundsCheck::runOnFunction(Function &F) {
     Inst = i;
     Builder->SetInsertPoint(Inst->getNextNode()); // insert after
     if (GetElementPtrInst *GEP = dyn_cast<GetElementPtrInst>(Inst)) {
-      MadeChange |= instrumentAddress(GEP, DL, GEP->getPointerOperand());
+      MadeChange |= instrumentGep(GEP);
     } else {
       llvm_unreachable("unknown Instruction type");
     }
@@ -444,22 +427,23 @@ bool FatBufferBoundsCheck::runOnFunction(Function &F) {
   for (Instruction *i : AccessWorkList) {
     Inst = i;
 
+    Value *RawPtr = nullptr;
     Builder->SetInsertPoint(Inst);
     if (LoadInst *LI = dyn_cast<LoadInst>(Inst)) {
-      MadeChange |= instrument(LI->getPointerOperand(), LI, DL);
-      LI->setOperand(LI->getPointerOperandIndex(), OrigPtr);
+      MadeChange |= instrument(LI->getPointerOperand(), LI, DL, RawPtr);
+      LI->setOperand(LI->getPointerOperandIndex(), RawPtr);
     } else if (StoreInst *SI = dyn_cast<StoreInst>(Inst)) {
-      MadeChange |=
-          instrument(SI->getPointerOperand(), SI->getValueOperand(), DL);
-      SI->setOperand(SI->getPointerOperandIndex(), OrigPtr);
+      MadeChange |= instrument(SI->getPointerOperand(), SI->getValueOperand(),
+                               DL, RawPtr);
+      SI->setOperand(SI->getPointerOperandIndex(), RawPtr);
     } else if (AtomicCmpXchgInst *AI = dyn_cast<AtomicCmpXchgInst>(Inst)) {
-      MadeChange |=
-          instrument(AI->getPointerOperand(), AI->getCompareOperand(), DL);
-      AI->setOperand(AI->getPointerOperandIndex(), OrigPtr);
+      MadeChange |= instrument(AI->getPointerOperand(), AI->getCompareOperand(),
+                               DL, RawPtr);
+      AI->setOperand(AI->getPointerOperandIndex(), RawPtr);
     } else if (AtomicRMWInst *AI = dyn_cast<AtomicRMWInst>(Inst)) {
       MadeChange |=
-          instrument(AI->getPointerOperand(), AI->getValOperand(), DL);
-      AI->setOperand(AI->getPointerOperandIndex(), OrigPtr);
+          instrument(AI->getPointerOperand(), AI->getValOperand(), DL, RawPtr);
+      AI->setOperand(AI->getPointerOperandIndex(), RawPtr);
     } else {
       llvm_unreachable("unknown Instruction type");
     }

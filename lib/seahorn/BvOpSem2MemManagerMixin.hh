3cc46c4c523695db0a4c7c2795af0bc3c8c6dd61
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-05-23 22:14:58 -0400

    fix(bvopsem2): using move semantics in BvOpSem2MemManagerMixin

--- lib/seahorn/BvOpSem2MemManagerMixin.hh
+++ lib/seahorn/BvOpSem2MemManagerMixin.hh
@@ -24,8 +24,8 @@ public:
 protected:
   Base &base() { return static_cast<Base &>(*this); }
   Base const &base() const { return static_cast<Base const &>(*this); }
-  auto toPtrTy(BasePtrTy &p) const { return static_cast<PtrTy>(p); }
-  auto toMemValTy(BaseMemValTy &m) const { return static_cast<MemValTy>(m); }
+  auto toPtrTy(BasePtrTy &&p) const { return static_cast<PtrTy>(p); }
+  auto toMemValTy(BaseMemValTy &&m) const { return static_cast<MemValTy>(m); }
 
 public:
   template <typename... Ts>
@@ -39,52 +39,52 @@ public:
 
   PtrTy salloc(unsigned bytes, uint32_t align = 0) override {
     auto res = base().salloc(bytes, align);
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   PtrTy salloc(Expr elmts, unsigned typeSz, uint32_t align = 0) override {
     auto res = base().salloc(elmts, typeSz, align);
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   PtrTy mkStackPtr(unsigned offset) override {
     auto res = base().mkStackPtr(offset);
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   PtrTy brk0Ptr() override {
     auto res = base().brk0Ptr();
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   PtrTy halloc(unsigned _bytes, uint32_t align = 0) override {
     auto res = base().halloc(_bytes, align);
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   PtrTy halloc(Expr bytes, uint32_t align = 0) override {
     auto res = base().halloc(bytes, align);
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   PtrTy galloc(const GlobalVariable &gv, uint32_t align = 0) override {
     auto res = base().galloc(gv, align);
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   PtrTy falloc(const Function &fn) override {
     auto res = base().falloc(fn);
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   PtrTy getPtrToFunction(const Function &F) override {
     auto res = base().getPtrToFunction(F);
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   PtrTy getPtrToGlobalVariable(const GlobalVariable &gv) override {
     auto res = base().getPtrToGlobalVariable(gv);
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   void initGlobalVariable(const GlobalVariable &gv) const {
@@ -93,7 +93,7 @@ public:
 
   PtrTy mkAlignedPtr(Expr name, uint32_t align) const override {
     auto res = base().mkAlignedPtr(name, align);
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   PtrSortTy mkPtrRegisterSort(const Instruction &inst) const override {
@@ -114,12 +114,12 @@ public:
 
   PtrTy freshPtr() override {
     auto res = base().freshPtr();
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   PtrTy nullPtr() const override {
     auto res = base().nullPtr();
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   // XXX figure out proper typing
@@ -128,12 +128,12 @@ public:
   /// \brief Pointer addition with symbolic offset
   PtrTy ptrAdd(PtrTy ptr, Expr offset) const override {
     auto res = base().ptrAdd(BasePtrTy(std::move(ptr)), offset);
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   PtrTy ptrAdd(PtrTy ptr, int32_t _offset) const override {
     auto res = base().ptrAdd(BasePtrTy(std::move(ptr)), _offset);
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   Expr loadIntFromMem(PtrTy ptr, MemValTy mem, unsigned byteSz,
@@ -147,7 +147,7 @@ public:
                        uint64_t align) override {
     auto res = base().loadPtrFromMem(
         BasePtrTy(std::move(ptr)), BaseMemValTy(std::move(mem)), byteSz, align);
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   MemValTy storeIntToMem(Expr _val, PtrTy ptr, MemValTy mem, unsigned byteSz,
@@ -155,7 +155,7 @@ public:
     auto res =
         base().storeIntToMem(_val, BasePtrTy(std::move(ptr)),
                              BaseMemValTy(std::move(mem)), byteSz, align);
-    return toMemValTy(res);
+    return toMemValTy(std::move(res));
   }
 
   MemValTy storePtrToMem(PtrTy val, PtrTy ptr, MemValTy mem, unsigned byteSz,
@@ -163,7 +163,7 @@ public:
     auto res =
         base().storePtrToMem(BasePtrTy(val), BasePtrTy(std::move(ptr)),
                              BaseMemValTy(std::move(mem)), byteSz, align);
-    return toMemValTy(res);
+    return toMemValTy(std::move(res));
   }
 
   Expr loadValueFromMem(PtrTy ptr, MemValTy mem, const llvm::Type &ty,
@@ -178,14 +178,14 @@ public:
     auto res =
         base().storeValueToMem(_val, BasePtrTy(std::move(ptr)),
                                BaseMemValTy(std::move(memIn)), ty, align);
-    return toMemValTy(res);
+    return toMemValTy(std::move(res));
   }
 
   MemValTy MemSet(PtrTy ptr, Expr _val, unsigned len, MemValTy mem,
                   uint32_t align) override {
     auto res = base().MemSet(BasePtrTy(std::move(ptr)), _val, len,
                              BaseMemValTy(std::move(mem)), align);
-    return toMemValTy(res);
+    return toMemValTy(std::move(res));
   }
 
   MemValTy MemCpy(PtrTy dPtr, PtrTy sPtr, unsigned len, MemValTy memTrsfrRead,
@@ -193,20 +193,20 @@ public:
     auto res =
         base().MemCpy(BasePtrTy(std::move(dPtr)), BasePtrTy(std::move(sPtr)),
                       len, BaseMemValTy(std::move(memTrsfrRead)), align);
-    return toMemValTy(res);
+    return toMemValTy(std::move(res));
   }
 
   MemValTy MemFill(PtrTy dPtr, char *sPtr, unsigned len, MemValTy mem,
                    uint32_t align = 0) override {
     auto res = base().MemFill(BasePtrTy(std::move(dPtr)), sPtr, len,
                               BaseMemValTy(std::move(mem)), align);
-    return toMemValTy(res);
+    return toMemValTy(std::move(res));
   }
 
   PtrTy inttoptr(Expr intVal, const Type &intTy,
                  const Type &ptrTy) const override {
     auto res = base().inttoptr(intVal, intTy, ptrTy);
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   Expr ptrtoint(PtrTy ptr, const Type &ptrTy,
@@ -253,7 +253,7 @@ public:
   PtrTy gep(PtrTy ptr, gep_type_iterator it,
             gep_type_iterator end) const override {
     auto res = base().gep(BasePtrTy(std::move(ptr)), it, end);
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 
   void onFunctionEntry(const Function &fn) override {
@@ -271,7 +271,7 @@ public:
 
   MemValTy zeroedMemory() const override {
     auto res = base().zeroedMemory();
-    return toMemValTy(res);
+    return toMemValTy(std::move(res));
   }
 
   Expr getFatData(PtrTy p, unsigned SlotIdx) override {
@@ -282,7 +282,7 @@ public:
   /// \brief returns Expr after setting data.
   PtrTy setFatData(PtrTy p, unsigned SlotIdx, Expr data) override {
     auto res = base().setFatData(BasePtrTy(std::move(p)), SlotIdx, data);
-    return toPtrTy(res);
+    return toPtrTy(std::move(res));
   }
 };
 } // namespace seahorn

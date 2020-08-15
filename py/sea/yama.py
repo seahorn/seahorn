121e83afb7443297724339fa043629050396b833
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-08-15 16:42:54 -0400

    feat(seapy): yama command for Yaml-based argument parser
    ```
    usage: sea yama [-h] [-y YCONFIG] [--dry-run]
    
    optional arguments:
      -h, --help  Print this message and exit
      -y YCONFIG  Configuration file
      --dry-run
    ```
    
    Reads a yaml dictionary, converts it to sea options, and re-runs sea with the
    new options. Multiple `-y` arguments can be given, in which case, options in the
    later file overwrite options in the prior file.
    
    Common usage pattern is:
    ```
    sea yama -y sea.yaml pf FILE.c
    ```
    
    where `sea.yaml` looks like the following
    ```
    sea_options:
      '-O3': ''
      inline: ''
      enable-loop-idiom: ''
      enable-indvar: ''
      no-lower-gv-init-struct: ''
      externalize-addr-taken-functions: ''
    ```
    
    Special key `command` is mapped to the command to execute. File to analyze must
    always be specified on the command line.
    
    Any extra options passed to `yama` are passed to the command line being
    constructed. For example, the command
    ```
    sea yama pf FILE.c
    ```
    is equivalent to
    ```
    sea pf FILE.c
    ```
    No configuration file was specified, so extra arguments are simply passed back
    to `sea`

---
+++ py/sea/yama.py
@@ -0,0 +1,121 @@
+import sys
+import sea
+
+class Yama(sea.CliCmd):
+    def __init__(self):
+        super().__init__('yama', 'Yama', allow_extra=True)
+
+    def mk_arg_parser(self, argp):
+       argp = super().mk_arg_parser(argp)
+
+       argp.add_argument("-y", dest='yconfig', action='append',
+                         help="Configuration file", nargs=1)
+       argp.add_argument('--dry-run', action='store_true', default=False)
+       return argp
+
+    def parse_yaml_options(self, fname):
+        import yaml
+        try:
+            with open(fname) as f:
+                data = yaml.load(f, Loader=yaml.SafeLoader)
+
+            # common containers for options
+            if 'verify_options' in data:
+                data = data['verify_options']
+            if 'sea_options' in data:
+                data = data['sea_options']
+            if 'sea_arguments' in data:
+                data = data['sea_arguments']
+            return data
+        except:
+            print("Error: could not parse", fname)
+            sys.exit(1)
+            return None
+
+    def mk_cli_from_key_value(self, _key, _value):
+        """
+        Convert (key, value) pair into a command line option
+        """
+        short_arg = False
+        key = str(_key)
+        # if key starts with a dash, it is a short form of an argument
+        if key.startswith('-'):
+            short_arg = True
+        else:
+            key = '--' + str(_key)
+            short_arg = False
+
+        # convert value to a string or None if it is not used (including empty string)
+        value = str(_value) if _value is not None else None
+        if value is not None and len(value) == 0:
+            value = None
+        if isinstance(_value, bool):
+            value = value.lower()
+
+        # short argument
+        if value is not None and short_arg:
+            return '{k} {v}'.format(k=key, v=value)
+        # long argument
+        elif value is not None and not short_arg:
+            return '{k}={v}'.format(k=key, v=value)
+        # flag only argument
+        else:
+            return key
+
+    def mk_cli_from_dict(self, arg_dict):
+        res = []
+        command = None
+        for key, value in arg_dict.items():
+            if key == 'command':
+                command = value
+            else:
+                res.append(self.mk_cli_from_key_value(key, value))
+
+        return command, res
+
+    def run(self, args = None, extra = []):
+        import sys
+        import os
+
+        # set default value
+        if args.yconfig is None:
+            args.yconfig = [['sea.yaml']]
+
+        args_dict = None
+        for f in args.yconfig:
+            assert(len(f) == 1)
+            yaml_args = self.parse_yaml_options(f[0])
+            if yaml_args is None:
+                continue
+            if args_dict is None:
+                args_dict = yaml_args
+            else:
+                args_dict.update(yaml_args)
+
+        cli = list()
+        if args_dict is not None:
+            command, cli = self.mk_cli_from_dict(args_dict)
+
+        # override command if specified on command line
+        if len(extra) > 0 and not extra[0].startswith('-'):
+            command = extra[0]
+            extra = extra[1:]
+
+        cmd_args = []
+        cmd_args.append(sys.argv[0])
+        cmd_args.append(command)
+        if len(cli) > 0:
+            cmd_args.extend(cli)
+        cmd_args.extend(extra)
+
+        print('Yama executing command:')
+        print(' '.join(cmd_args))
+
+        sys.stdout.flush()
+        sys.stderr.flush()
+        if args.dry_run:
+            return 0
+        os.execl(sys.executable, sys.executable, *cmd_args)
+        # unreachable, unless there is an error
+        return 1
+

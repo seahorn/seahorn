2973ed0b44ee838407c302a2a167766e0fc27ca2
Siddharth Priya <priya.siddharth@gmail.com>
2020-11-04 16:10:31 -0500

    feat(opsem): add instrinsics for {reset,is}_modified
    TODO: add intrinics for getBase(), getSize() etc. in subsequent PR.

--- include/seahorn/Transforms/Scalar/PromoteVerifierCalls.hh
+++ include/seahorn/Transforms/Scalar/PromoteVerifierCalls.hh
@@ -13,24 +13,25 @@ namespace seahorn
 {
   using namespace llvm;
   
-  struct PromoteVerifierCalls : public ModulePass
-  {
+  struct PromoteVerifierCalls : public ModulePass {
     static char ID;
-    
+
     Function *m_assumeFn;
     Function *m_assertFn;
     Function *m_assertNotFn;
     Function *m_errorFn;
-    Function *m_failureFn;  // to indicate failure. It can only appears in main.
+    Function *m_failureFn; // to indicate failure. It can only appears in main.
     Function *m_is_deref;
     Function *m_assert_if;
+    Function *m_is_modified;
+    Function *m_reset_modified;
 
-    PromoteVerifierCalls () : ModulePass (ID) {}
-    
-    bool runOnModule (Module &M);
-    bool runOnFunction (Function &F);
-    void getAnalysisUsage (AnalysisUsage &AU) const;
-    virtual StringRef getPassName () const {return "PromoteVerifierCalls";}
+    PromoteVerifierCalls() : ModulePass(ID) {}
+
+    bool runOnModule(Module &M);
+    bool runOnFunction(Function &F);
+    void getAnalysisUsage(AnalysisUsage &AU) const;
+    virtual StringRef getPassName() const { return "PromoteVerifierCalls"; }
   };
 }
 

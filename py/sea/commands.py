8e580de9d569a64828e68391dedd119b820d6476
Daniel Zhou <danzhou1810@gmail.com>
2021-01-05 16:03:32 -0500

    feat(seapy): add sea cex pipeline; separate cex cmds file

--- py/sea/commands.py
+++ py/sea/commands.py
@@ -203,7 +203,7 @@ class LinkRt(sea.LimitedCmd):
 
     def name_out_file (self, in_files, args=None, work_dir=None):
         assert (len (in_files) == 1)
-        return _remap_file (in_files[0], '', work_dir)
+        return _remap_file_name (in_files[0], '', work_dir)
 
     def run (self, args, extra):
 
@@ -744,37 +744,6 @@ class WrapMem(sea.LimitedCmd):
             if args.llvm_asm: argv.append ('-S')
             argv.extend (args.in_files)
             return self.seappCmd.run (args, argv)
-
-class ExecHarness(sea.LimitedCmd):
-    def __init__(self, quiet=False):
-        super(ExecHarness, self).__init__('exec-harness', 'Execute an executable harness to see if it calls __VERIFIER_error',
-                                             allow_extra=True)
-        self.harnessCmd = None
-
-    @property
-    def stdout (self):
-        return self.harnessCmd.stdout
-
-    def mk_arg_parser (self, ap):
-        ap = super (ExecHarness, self).mk_arg_parser (ap)
-        add_in_args (ap)
-        return ap
-
-    def run (self, args, extra):
-        if len(args.in_files) != 1: raise IOError('ExecHarness expects the harness executable as an input')
-        eharness = os.path.abspath(args.in_files[0])
-
-        self.harnessCmd = sea.ExtCmd (eharness)
-        if args.cpu is None: args.cpu = 10
-
-        retval = self.harnessCmd.run (args, [eharness], encoding='utf-8', errors='replace', stdout=subprocess.PIPE)
-
-        if "[sea] __VERIFIER_error was executed" in self.harnessCmd.stdout:
-            print ("__VERIFIER_error was executed")
-        else:
-            print ("__VERIFIER_error was not executed")
-
-        return retval
 
 class CutLoops(sea.LimitedCmd):
     def __init__(self, quiet=False):
@@ -1610,118 +1579,6 @@ class InspectBitcode(sea.LimitedCmd):
 
         return self.seainspectCmd.run (args, argv)
 
-class SeaExeCex(sea.LimitedCmd):
-    def __init__(self, quiet=False):
-        super (SeaExeCex, self).__init__('exe-cex',
-                                         'Executable Counterexamples',
-                                         allow_extra=True)
-    @property
-    def stdout (self):
-        return
-
-    def name_out_file (self, in_files, args=None, work_dir=None):
-        return _remap_file_name (in_files[0], '.exe', work_dir)
-
-    def mk_arg_parser (self, ap):
-        ap = super (SeaExeCex, self).mk_arg_parser (ap)
-        add_tmp_dir_args (ap)
-        add_in_out_args (ap)
-        _add_S_arg (ap)
-        ap.add_argument ('--harness', '-harness',
-                         dest='harness',
-                         help='Destination file for the harness',
-                         default=None, metavar='FILE')
-        ap.add_argument ('--bit-precise','-bit-precise',
-                         dest='bv_cex',
-                         help='Generate bit-precise counterexamples',
-                         default=False, action='store_true')
-        ap.add_argument ('--bit-mem-precise','-bit-mem-precise',
-                         dest='bv_part_mem',
-                         help='Generate bit- and memory-precise counterexamples',
-                         default=False, action='store_true')
-        ap.add_argument ('--alloc-mem', '-alloc-mem',
-                         default=False, action='store_true',
-                         dest='alloc_mem',
-                         help='Allocate space for uninitialized memory')
-        ap.add_argument ('--verify', '-verify',
-                         default=False, action='store_true',
-                         dest='verify',
-                         help='Verify the harness executes __VERIFIER_error')
-        return ap
-
-    def run(self, args, extra):
-        # FIXME: the -o is used both `sea pf` and `sea exe`. Thus,
-        # the output of `sea pf` is overwritten by the output of `sea exe`.
-        try:
-            harness_file = args.harness
-            if harness_file is None:
-                harness_file = 'harness.ll'
-
-            if args.out_file is None:
-                args.out_file = 'harness'
-
-            try:
-                os.remove(harness_file)
-            except OSError:
-                pass
-
-            ## `sea pf`: find counterexample and generate harness
-            c = sea.SeqCmd('','',
-                           [Clang(), Seapp(), MixedSem(), Seaopt(), \
-                            Seahorn(solve=True)])
-            argv = list ()
-            argv.extend(['-m64', '-g'])
-            argv.extend(extra)
-            # TODO: we should remove from `extra` the following options:
-            argv.extend(['--cex={0}'.format(harness_file)])
-            if args.bv_cex or args.bv_part_mem:
-                argv.extend(['--bv-cex'])
-            if args.bv_part_mem:
-                argv.extend(['--horn-bv-part-mem'])
-            res = c.run(args,  argv)
-            if res != 0:
-                print('\n\nThe harness was not generated. Aborting ...\n');
-                return res
-
-            if not os.path.isfile(harness_file): # if program was safe
-                print('\n\nThe harness was not generated. Aborting ...\n');
-                # remove the .smt2 file
-                try:
-                    os.remove(args.out_file)
-                except OSError:
-                    pass
-                # some error code different from 0
-                return 1
-
-
-            ## `sea exe`: generate executable
-            c = sea.SeqCmd('','',
-                           [Clang(), Seapp(strip_extern=True,keep_lib_fn=True), \
-                            Seapp(internalize=True), WrapMem(), LinkRt()])
-            argv = list()
-            argv.extend(['-m64', '-g'])
-            argv.extend(extra)
-            # TODO: we should remove from `extra` the following options:
-            if args.alloc_mem:
-                argv.extend(['--alloc-mem'])
-            infiles = args.in_files
-            infiles.extend([harness_file])
-            res = c.run(args, argv)
-            if res != 0:
-                print('\n\nThe executable counterexample was not generated. Aborting ...\n');
-            else:
-                print ('\n\nGenerated executable counterexample {0}'.format(args.out_file))
-
-            # Optionally verify the harness reaches __VERIFIER_error
-            if res == 0 and args.verify:
-                args.in_files = [args.out_file]
-                c = ExecHarness()
-                c.run(args, [])
-
-            return res
-
-        except Exception as e:
-            raise IOError(str(e))
 
 ## SeaHorn aliases
 FrontEnd = sea.SeqCmd ('fe', 'Front end: alias for clang|pp|ms|opt',

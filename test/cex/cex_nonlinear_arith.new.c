22db1d3b1ff9cfac1dd185f07a7c8f5a0124eac9
Daniel Zhou <danzhou1810@gmail.com>
2021-01-05 16:05:54 -0500

    test(cex): add more memhavoc test cases

--- test/cex/cex_nonlinear_arith.new.c
+++ test/cex/cex_nonlinear_arith.new.c
@@ -1,7 +1,10 @@
-// RUN: %sea exe-cex -O0 --verify --bit-precise "%s" 2>&1 | OutputCheck %s
+// RUN: %solve --horn-bmc-engine=mono --horn-bv2-lambdas=false --horn-bmc --horn-bv2=true --cex=/tmp/test_cex_nonlinear_arith.ll %s > /dev/null 2>&1
+// RUN: %cex --run -g %s /tmp/test_cex_nonlinear_arith.ll 2>&1 | OutputCheck %s
+
 // CHECK: ^__VERIFIER_error was executed$
 
 /* 
+   XXX: how to reproduce spurious cex?
    Example where SeaHorn's back-end solver produces an spurious cex
    which is not valid at the bit-level. The reason is that the solver
    does not reason about non-linear multiplication.
@@ -13,8 +16,8 @@ extern int nd_int(void);
 
 int main(int argc, char**argv) {
   int x = nd_int();
-  __VERIFIER_assume (x >= 1);
-  __VERIFIER_assume (x <= 100);
+  assume (x >= 1);
+  assume (x <= 100);
   int y = x * x;
   if (y > x) {
     __VERIFIER_error();

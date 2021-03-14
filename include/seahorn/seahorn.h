272eaf31d4b50f8e1db4f678c2c4d2f59609556b
Arie Gurfinkel <arie.gurfinkel@gmail.com>
2021-03-13 22:30:39 -0500

    fix(seahorn.h): warnings on prototypes

--- include/seahorn/seahorn.h
+++ include/seahorn/seahorn.h
@@ -38,9 +38,9 @@ extern void sea_assert_if(bool, bool);
  */
 extern bool sea_is_modified(char *);
 /* tracking is set to on for subsequent program till exit or sea_tracking_off */
-extern void sea_tracking_on();
+extern void sea_tracking_on(void);
 /* tracking is set to off for subsequent program till exit or sea_tracking_on */
-extern void sea_tracking_off();
+extern void sea_tracking_off(void);
 /* reset modified metadata for memory pointed to by arg */
 extern void sea_reset_modified(char *);
 #ifdef __cplusplus

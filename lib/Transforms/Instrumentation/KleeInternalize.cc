a1f1c6887b29e99b3831b0b8e0a597b9ee91568a
scottwe <aswesley@uwaterloo.ca>
2021-06-03 20:13:18 -0400

    chore(transforms): clang-format on KleeInternalize.cc

--- lib/Transforms/Instrumentation/KleeInternalize.cc
+++ lib/Transforms/Instrumentation/KleeInternalize.cc
@@ -12,11 +12,11 @@ terms.
 #include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
 
+#include "seahorn/Support/SeaDebug.h"
 #include "llvm/Analysis/CallGraph.h"
 #include "llvm/Analysis/TargetLibraryInfo.h"
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/CommandLine.h"
-#include "seahorn/Support/SeaDebug.h"
+#include "llvm/Support/raw_ostream.h"
 
 #include <fstream>
 #include <set>

cc6e253a604a0546ea998caa3c17be82fc4f11c5
scottwe <aswesley@uwaterloo.ca>
2021-06-03 20:07:56 -0400

    chore(transforms): clang-format on MarkInternalInline.cc

--- lib/Transforms/Utils/MarkInternalInline.cc
+++ lib/Transforms/Utils/MarkInternalInline.cc
@@ -1,61 +1,57 @@
-#include "llvm/Pass.h"
-#include "llvm/IR/Module.h"
 #include "llvm/ADT/SmallSet.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/Support/CommandLine.h"
 
-#include "seahorn/Support/SeaDebug.h"
 #include "boost/range.hpp"
+#include "seahorn/Support/SeaDebug.h"
 
 using namespace llvm;
 
 static llvm::cl::list<std::string>
-InlineOnly("horn-inline-only",
-           llvm::cl::desc("Inline only selected functions"),
-           llvm::cl::ZeroOrMore, llvm::cl::CommaSeparated);
+    InlineOnly("horn-inline-only",
+               llvm::cl::desc("Inline only selected functions"),
+               llvm::cl::ZeroOrMore, llvm::cl::CommaSeparated);
 
-namespace seahorn
-{
-  /// marks all internal functions with AlwaysInline attribute
-  struct MarkInternalInline : public ModulePass
-  {
-    static char ID;
-    MarkInternalInline () : ModulePass (ID) {}
+namespace seahorn {
+/// marks all internal functions with AlwaysInline attribute
+struct MarkInternalInline : public ModulePass {
+  static char ID;
+  MarkInternalInline() : ModulePass(ID) {}
 
-    virtual StringRef getPassName () const 
-    {return "Mark all internal functions with AlwaysInline attribute";}
-    
-    void getAnalysisUsage (AnalysisUsage &AU) const
-    {AU.setPreservesAll ();}
-    
-    bool runOnModule (Module &M)
-    {
-      SmallSet<Function*,8> selectedFn;
-      for (auto &fname: boost::make_iterator_range(InlineOnly.begin(),InlineOnly.end()))
-        if (Function*F = M.getFunction(fname))
-          selectedFn.insert (F);
+  virtual StringRef getPassName() const {
+    return "Mark all internal functions with AlwaysInline attribute";
+  }
 
-      for (Function &F : M) {
-        if (!F.isDeclaration () && F.hasLocalLinkage ()) {
-          if (selectedFn.empty () || selectedFn.count (&F) > 0) {
-            LOG("inline", 
-                errs () << "INLINED " << F.getName () << "\n");
+  void getAnalysisUsage(AnalysisUsage &AU) const { AU.setPreservesAll(); }
 
-	    if (F.hasFnAttribute(Attribute::NoInline)) {
-	      F.removeFnAttr(Attribute::NoInline);
-	    }
-            F.addFnAttr (Attribute::AlwaysInline);
+  bool runOnModule(Module &M) {
+    SmallSet<Function *, 8> selectedFn;
+    for (auto &fname :
+         boost::make_iterator_range(InlineOnly.begin(), InlineOnly.end()))
+      if (Function *F = M.getFunction(fname))
+        selectedFn.insert(F);
+
+    for (Function &F : M) {
+      if (!F.isDeclaration() && F.hasLocalLinkage()) {
+        if (selectedFn.empty() || selectedFn.count(&F) > 0) {
+          LOG("inline", errs() << "INLINED " << F.getName() << "\n");
+
+          if (F.hasFnAttribute(Attribute::NoInline)) {
+            F.removeFnAttr(Attribute::NoInline);
           }
+          F.addFnAttr(Attribute::AlwaysInline);
         }
       }
-      return true;
     }
-    
-  };
-  
-  char MarkInternalInline::ID = 0;
-  
-  Pass *createMarkInternalInlinePass () {return new MarkInternalInline ();}
-  
-}
+    return true;
+  }
+};
+
+char MarkInternalInline::ID = 0;
+
+Pass *createMarkInternalInlinePass() { return new MarkInternalInline(); }
+
+} // namespace seahorn

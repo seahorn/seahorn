29e3eec12bd5e6c07d671532ca67cda444cbbd9d
Xiang Zhou <x245zhou@uwaterloo.ca>
2021-09-07 00:42:38 -0400

    feat(mem-repr): add ITE rule for if condition is true or false

--- include/seahorn/Expr/ExprRewriteRule.hh
+++ include/seahorn/Expr/ExprRewriteRule.hh
@@ -56,15 +56,23 @@ struct ITERewriteRule : public ExprRewriteRule {
     }
     // ite(a, false, true) => !a
     if (isOpX<FALSE>(t) && isOpX<TRUE>(e)) {
-      return {mk<NEG>(i), rewrite_status::RW_2}; // simp dbl negation
+      return {mk<NEG>(i), rewrite_status::RW_1}; // simp dbl negation
     }
     // ite(a, b, ite(!a, c, d)) => ite(a, b, c)
     if (isOpX<ITE>(e)) {
       Expr e_i = e->arg(0);
       Expr e_t = e->arg(1);
       if (op::boolop::areNegations(i, e_i)) {
-        return {mk<ITE>(i, t, e_t), rewrite_status::RW_DONE};
+        return {mk<ITE>(i, t, e_t), rewrite_status::RW_1};
       }
+    }
+    // ite(true, a, b) => a
+    if (isOpX<TRUE>(i)) {
+      return {t, rewrite_status::RW_1};
+    }
+    // ite(false, a, b) => b
+    if (isOpX<FALSE>(i)) {
+      return {e, rewrite_status::RW_1};
     }
     return {exp, rewrite_status::RW_SKIP};
   }

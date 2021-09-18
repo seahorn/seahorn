ecb9605f94031429f4d76c344d1b62cc8075f7e4
Xiang Zhou <x245zhou@uwaterloo.ca>
2021-09-17 21:01:44 -0400

    feat(mem-repr): iterative push select; generalized bvadd
    (to-squash): handle ites as well
    
    feat(to-squash): bvadd rules finalized

--- lib/seahorn/Smt/ExprRewriter.cc
+++ lib/seahorn/Smt/ExprRewriter.cc
@@ -5,6 +5,58 @@ namespace expr {
 
 namespace utils {
 bool shouldCache(Expr e) { return e->use_count() > 1; }
+
+Expr pushSelectDownStoreITE(Expr arr, Expr idx, ExprFactory &efac) {
+  if (!isOpX<STORE>(arr) && !isOpX<ITE>(arr)) {
+    return op::array::select(arr, idx);
+  }
+  Expr res;          // final rewritten ITE
+  ExprVector nested; // nested store/ITEs being selected from
+  Expr child = arr;
+  while (isOpX<STORE>(child) || isOpX<ITE>(child)) {
+    nested.push_back(child);
+    size_t childIdx = isOpX<STORE>(child) ? 0 : 1;
+    child = child->arg(childIdx);
+  }
+
+  Expr back = nested.back();
+  if (isOpX<STORE>(back)) {
+    /** leaf case for *back* is store(arrn, idxn, valn):
+     * ite(idx == idxn, valn, select(arrn, idx))
+     **/
+    Expr arrN = back->arg(0);
+    Expr idxN = back->arg(1);
+    Expr valN = back->arg(2);
+    res = mk<ITE>(mk<EQ>(idx, idxN), valN, op::array::select(arrN, idx));
+  } else {
+    /** must be ITE.
+     * leaf case for *back* is ite(iN, tN, eN):
+     * ite(iN, select(tN, idx), select(eN, idx))
+     **/
+    Expr iN = back->arg(0);
+    Expr tN = back->arg(1);
+    Expr eN = back->arg(2);
+    res = mk<ITE>(iN, op::array::select(tN, idx), op::array::select(eN, idx));
+  }
+  nested.pop_back();
+  // construct ITE from btm up
+  while (!nested.empty()) {
+    back = nested.back();
+    if (isOpX<STORE>(back)) {
+      /** node case: store(rewritten, idxN, valN) =>
+       * ite(idx == idxN, valN, rewritten) **/
+      res = mk<ITE>(mk<EQ>(idx, back->arg(1)), back->arg(2), res);
+    } else {
+      /** must be ITE.
+       * node case: ite(iN, rewritten, eN) =>
+       * ite(iN, rewritten, select(eN, idx))
+       **/
+      res = mk<ITE>(back->arg(0), res, op::array::select(back->arg(2), idx));
+    }
+    nested.pop_back();
+  }
+  return res;
+}
 } // namespace utils
 
 Expr rewriteITEComp(Expr exp) {
@@ -25,7 +77,7 @@ rewrite_result ITECompRewriteConfig::applyRewriteRules(Expr exp) {
     res = m_compRule(exp);
   } else if (isOpX<BoolOp>(exp)) {
     res = m_boolRule(exp);
-  } else if (isOpX<STORE>(exp) || isOpX<SELECT>(exp)) {
+  } else if (isOpX<SELECT>(exp)) {
     res = m_arrayRule(exp);
   } else if (isOpX<BADD>(exp)) {
     res = m_arithRule(exp);

37125f69d8dd358e2dd4201327b0eb4c49cfb4a2
Siddharth Priya <priya.siddharth@gmail.com>
2022-11-07 09:10:01 -0500

    chore: seapy uses clang-14 now

--- py/sea/commands.py
+++ py/sea/commands.py
@@ -78,7 +78,7 @@ class Clang(sea.LimitedCmd):
         def link_bc_files(bc_files, args):
             if len(bc_files) <= 1:
                 return 0
-            cmd_name = which (['llvm-link-mp-12', 'llvm-link-12', 'llvm-link'])
+            cmd_name = which(['llvm-link-mp-14', 'llvm-link-14', 'llvm-link'])
             if cmd_name is None: raise IOError ('llvm-link not found')
             self.linkCmd = sea.ExtCmd (cmd_name,'',quiet)
 
@@ -106,9 +106,9 @@ class Clang(sea.LimitedCmd):
             return link_bc_files(out_files, args)
 
         if self.plusplus:
-            cmd_name = which (['clang++-mp-12', 'clang++-12', 'clang++'])
+            cmd_name = which(['clang++-mp-14', 'clang++-14', 'clang++'])
         else:
-            cmd_name = which (['clang-mp-12', 'clang-12', 'clang'])
+            cmd_name = which(['clang-mp-14', 'clang-14', 'clang'])
 
         if cmd_name is None: raise IOError ('clang not found')
         self.clangCmd = sea.ExtCmd (cmd_name,'',quiet)
@@ -210,7 +210,7 @@ class LinkRt(sea.LimitedCmd):
 
     def run (self, args, extra):
 
-        cmd_name = which (['clang++-mp-12', 'clang++-12', 'clang++'])
+        cmd_name = which(['clang++-mp-14', 'clang++-14', 'clang++'])
 
         if cmd_name is None: raise IOError ('clang++ not found')
         self.clangCmd = sea.ExtCmd (cmd_name,'',quiet)

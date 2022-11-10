7f4d7e444194fafcb115e5fc17d575a7ec4e8d7f
caballa <navasjorgea@gmail.com>
2022-11-10 10:19:36 -0700

    chore: updated test after porting to LLVM14

--- test/crab/crab_is_deref_2.c
+++ test/crab/crab_is_deref_2.c
@@ -1,7 +1,9 @@
-// RUN: %sea -O3 fpf --inline --horn-bv2-crab-lower-is-deref --horn-bmc-crab-dom=zones --dsa=sea-cs-t --bmc=opsem --horn-vcgen-use-ite --horn-vcgen-only-dataflow=false --horn-bmc-coi=false --horn-stats=true --sea-opsem-allocator=static "%s" 2>&1 | OutputCheck %s
-// RUN: %sea -O3 fpf --inline --horn-bv2-crab-check-is-deref --horn-bmc-crab-dom=zones --dsa=sea-cs-t --bmc=opsem --horn-vcgen-use-ite --horn-vcgen-only-dataflow=false --horn-bmc-coi=false --horn-stats=true --sea-opsem-allocator=static "%s" 2>&1 | OutputCheck %s
+// RUN: %sea -O3 fpf --bound=5 --inline --horn-bv2-crab-lower-is-deref --horn-bmc-crab-dom=zones --dsa=sea-cs-t --bmc=opsem --horn-vcgen-use-ite --horn-vcgen-only-dataflow=false --horn-bmc-coi=false --horn-stats=true --sea-opsem-allocator=static "%s" 2>&1 | OutputCheck %s
+// RUN: %sea -O3 fpf --bound=5 --inline --horn-bv2-crab-check-is-deref --horn-bmc-crab-dom=zones --dsa=sea-cs-t --bmc=opsem --horn-vcgen-use-ite --horn-vcgen-only-dataflow=false --horn-bmc-coi=false --horn-stats=true --sea-opsem-allocator=static "%s" 2>&1 | OutputCheck %s
 // CHECK: ^unsat$
 // CHECK: crab.isderef.solve 6
+
+/** Crab proves all the sea_is_dereferenceable checks **/
 
 #include <seahorn/seahorn.h>
 #include <stdio.h>
@@ -21,12 +23,12 @@ int main() {
     int max_len = 1024;
     assume(len < max_len);
     uint8_t *ptr = malloc(len);
+    memhavoc(ptr, len);
     sassert(sea_is_dereferenceable(ptr, len));
     while (len > 0) {
         uint8_t *p2 = ptr + (len - 1);
         sassert(sea_is_dereferenceable(p2, sizeof(uint8_t)));
-        uint8_t tmp = *p2;
-        -- len;
+        --len;
     }
    return 0;
 }

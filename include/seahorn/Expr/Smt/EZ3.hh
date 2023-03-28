8a4b61f05d71026abb82232f655c42cbf6391dcf
Arie Gurfinkel <arie.gurfinkel@gmail.com>
2023-03-28 11:56:56 -0400

    chore(expr): minor cleanup, mostly names, a few datastructures
    Changed std::vector to llvm::SmallVector and a few formatting changes. No
    expected change to functionality. Expected a slight improvement to
    performance (but unlikely to be noticeable in most applications).

--- include/seahorn/Expr/Smt/EZ3.hh
+++ include/seahorn/Expr/Smt/EZ3.hh
@@ -2,5 +2,9 @@
 #include "seahorn/Expr/Smt/ExprToZ.hh"
 
 namespace seahorn {
+#ifdef SEA_REC_ZEXPR
 using EZ3 = ZContext<ExprToZ, ZToExpr>;
+#else
+using EZ3 = ZContext<ExprToZ, ZToExprNoRec>;
+#endif
 }
